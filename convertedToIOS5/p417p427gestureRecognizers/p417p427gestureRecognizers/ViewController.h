3d4e75e0f6c30d8dfdbd4dc9115fe21b5c991e4a
Matt Neuburg <matt@tidbits.com>
2011-10-31 10:07:39 -0700

    converted to iOS 5 thru end of Part IV

---
+++ convertedToIOS5/p417p427gestureRecognizers/p417p427gestureRecognizers/ViewController.h
@@ -0,0 +1,7 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (nonatomic, strong) IBOutlet UIView* v;
+@end
---
+++ convertedToIOS5/p420p430gestureRecognizerDelegate/p420p430gestureRecognizerDelegate/ViewController.h
@@ -0,0 +1,8 @@
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (nonatomic, strong) IBOutlet UIView* v;
+@property (nonatomic, strong) UILongPressGestureRecognizer* longPresser;
+
+@end
