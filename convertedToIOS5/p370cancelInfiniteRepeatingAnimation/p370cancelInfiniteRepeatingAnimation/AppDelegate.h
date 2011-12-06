f2795f78dddd0ccae69f2b60fa070e6815826bed
Matt Neuburg <matt@tidbits.com>
2011-12-05 20:27:58 -0800

    new examples about repeating block-based animations

---
+++ convertedToIOS5/p370cancelInfiniteRepeatingAnimation/p370cancelInfiniteRepeatingAnimation/AppDelegate.h
@@ -0,0 +1,11 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIView* v;
+@property (strong, nonatomic) UIButton* button;
+
+@end
