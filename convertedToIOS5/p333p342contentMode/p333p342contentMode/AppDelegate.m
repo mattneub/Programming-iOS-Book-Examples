d78d4f1efcfb66b325701852980830f88ebf1a86
Matt Neuburg <matt@tidbits.com>
2011-12-07 09:37:12 -0800

    added note about using catransaction instead of delayed performance to wait until after redraw moment

--- convertedToIOS5/p333p342contentMode/p333p342contentMode/AppDelegate.m
+++ convertedToIOS5/p333p342contentMode/p333p342contentMode/AppDelegate.m
@@ -2,10 +2,18 @@
 
 #import "AppDelegate.h"
 #import "MyView.h"
+#import <QuartzCore/QuartzCore.h>
 
 @implementation AppDelegate
 
 @synthesize window = _window;
+
+- (void) resize: (id) dummy {
+    UIView* mv = [self.window viewWithTag:111];
+    CGRect f = mv.bounds;
+    f.size.height *= 2;
+    mv.bounds = f;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -20,8 +28,8 @@
     mv.tag = 111; // so I can get a reference to this view later
 
     [self performSelector:@selector(resize:) withObject:nil afterDelay:0.1];
-
-    
+    // NOTE: this is another way of doing the same thing, i.e. wait until after redraw moment
+    // [CATransaction setCompletionBlock:^{[self resize:nil];}];
     
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
@@ -29,12 +37,6 @@
     return YES;
 }
 
-- (void) resize: (id) dummy {
-    UIView* mv = [self.window viewWithTag:111];
-    CGRect f = mv.bounds;
-    f.size.height *= 2;
-    mv.bounds = f;
-}
 
 
 @end
