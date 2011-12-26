9c9111e7ae1e1cb107fe4cb74601b217f6c0c787
Matt Neuburg <matt@tidbits.com>
2011-12-25 20:43:46 -0800

    new example showing table cell background layering

---
+++ convertedToIOS5/p511cellBackgroundLayering/p511cellBackgroundLayering/AppDelegate.m
@@ -0,0 +1,21 @@
+
+
+#import "AppDelegate.h"
+#import "RootViewController.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.rootViewController = [[RootViewController alloc] initWithNibName:@"RootViewController" bundle:nil];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+
+@end
