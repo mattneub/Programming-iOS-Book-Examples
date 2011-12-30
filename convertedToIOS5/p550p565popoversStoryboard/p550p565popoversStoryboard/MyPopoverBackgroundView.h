fa7eeb750b65994d6252340316ab4abd64cbef4d
Matt Neuburg <matt@tidbits.com>
2011-12-29 21:00:01 -0800

    replaced popover storyboard example to demonstrate popover segue

---
+++ convertedToIOS5/p550p565popoversStoryboard/p550p565popoversStoryboard/MyPopoverBackgroundView.h
@@ -0,0 +1,7 @@
+
+#import <UIKit/UIKit.h>
+#import <UIKit/UIPopoverBackgroundView.h> // must explicitly import this; is this a bug?
+
+@interface MyPopoverBackgroundView : UIPopoverBackgroundView
+
+@end
