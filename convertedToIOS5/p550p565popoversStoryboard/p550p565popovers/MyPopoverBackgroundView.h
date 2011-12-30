acd8e7c762039111cbd296584a5a1b47e65f05cf
Matt Neuburg <matt@tidbits.com>
2011-12-30 09:35:10 -0800

    oops, forgot to delete the deleted files

--- convertedToIOS5/p550p565popoversStoryboard/p550p565popovers/MyPopoverBackgroundView.h
+++
@@ -1,7 +0,0 @@
-
-#import <UIKit/UIKit.h>
-#import <UIKit/UIPopoverBackgroundView.h> // must explicitly import this; is this a bug?
-
-@interface MyPopoverBackgroundView : UIPopoverBackgroundView
-
-@end
