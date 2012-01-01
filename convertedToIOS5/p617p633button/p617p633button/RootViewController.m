178ec8c6ec7b35e0873cca356e93a6d94c62f163
Matt Neuburg <matt@tidbits.com>
2012-01-01 12:38:41 -0800

    modernized the button example a little more

--- convertedToIOS5/p617p633button/p617p633button/RootViewController.m
+++ convertedToIOS5/p617p633button/p617p633button/RootViewController.m
@@ -12,8 +12,7 @@
 -(void)viewDidLoad {
     UIImage* im = [UIImage imageNamed: @"coin2.png"];
     CGSize sz = [im size];
-    UIImage* im2 = [im stretchableImageWithLeftCapWidth:sz.width/2.0 
-                                           topCapHeight:sz.height/2.0];
+    UIImage* im2 = [im resizableImageWithCapInsets:UIEdgeInsetsMake(sz.height/2.0, sz.width/2.0, sz.height/2.0, sz.width/2.0)];
     [button setBackgroundImage: im2 forState: UIControlStateNormal];
     button.backgroundColor = [UIColor clearColor];
 }
