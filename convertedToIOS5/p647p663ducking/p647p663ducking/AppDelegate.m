2cd3f24bfb187aa807b76fb0fe5263ddc66781bc
Matt Neuburg <matt@tidbits.com>
2012-01-26 19:21:20 -0800

    converted chapter 27 examples

---
+++ convertedToIOS5/p647p663ducking/p647p663ducking/AppDelegate.m
@@ -0,0 +1,33 @@
+
+
+#import "AppDelegate.h"
+
+#import "ViewController.h"
+
+#import <AVFoundation/AVFoundation.h>
+
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+@synthesize viewController = _viewController;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryAmbient error: NULL];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    [[AVAudioSession sharedInstance] setActive: YES error: NULL];
+}
+
+
+@end
