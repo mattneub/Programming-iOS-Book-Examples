a5aff0a49f113b1fed470d7fcc6c23e8633527d4
Matt Neuburg <matt@tidbits.com>
2012-02-01 18:46:09 -0800

    finished AV Foundation camera capture example

--- convertedToIOS5/p683cameraCaptureWithAVFoundation/p683cameraCaptureWithAVFoundation/ViewController.m
+++ convertedToIOS5/p683cameraCaptureWithAVFoundation/p683cameraCaptureWithAVFoundation/ViewController.m
@@ -4,10 +4,11 @@
 
 @interface ViewController()
 @property (nonatomic, strong) AVCaptureSession* sess;
+@property (nonatomic, strong) AVCaptureStillImageOutput* snapper;
 @end
 
 @implementation ViewController
-@synthesize sess=_sess;
+@synthesize sess=_sess, snapper=_snapper;
 
 - (IBAction)doStart:(id)sender {
     if (self.sess && self.sess.isRunning) {
@@ -17,19 +18,54 @@
     }
     
     self.sess = [AVCaptureSession new];
+    
+    self.sess.sessionPreset = AVCaptureSessionPreset640x480;
+    self.snapper = [AVCaptureStillImageOutput new];
+    self.snapper.outputSettings = [NSDictionary dictionaryWithObject:AVVideoCodecJPEG 
+                                                              forKey:AVVideoCodecKey];
+    [self.sess addOutput:self.snapper];
+
     AVCaptureDevice* cam = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     AVCaptureDeviceInput* input = [AVCaptureDeviceInput deviceInputWithDevice:cam error:nil];
     [self.sess addInput:input];
     
     AVCaptureVideoPreviewLayer* lay = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.sess];
-    lay.videoGravity = AVLayerVideoGravityResizeAspect;
-    lay.masksToBounds = YES;
-    lay.frame = CGRectMake(0,0,300,300);
-    lay.backgroundColor = [UIColor blackColor].CGColor;
-    lay.position = CGPointMake(self.view.bounds.size.width/2.0, 160);
+    lay.frame = CGRectMake(10,30,300,300);
     [self.view.layer addSublayer:lay];
     
     [self.sess startRunning];
+}
+
+- (IBAction)doSnap:(id)sender {
+    // find AVCaptureConnection; copied from Apple's code
+    AVCaptureConnection *vc = nil;
+    for (AVCaptureConnection *con in self.snapper.connections) {
+        for (AVCaptureInputPort *port in [con inputPorts]) {
+            if ([[port mediaType] isEqual:AVMediaTypeVideo] ) {
+                vc = con;
+                break;
+            }
+        }
+        if (vc)
+            break;
+    }
+    // deal with image when it arrives
+    typedef void(^MyBufBlock)(CMSampleBufferRef, NSError*);
+    MyBufBlock h = ^(CMSampleBufferRef buf, NSError *err) {
+        NSData* data = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:buf];
+        UIImage* im = [UIImage imageWithData:data];
+        NSLog(@"%@", NSStringFromCGSize(im.size));
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIImageView* iv = [[UIImageView alloc] initWithFrame:CGRectMake(10,30,300,300)];
+            iv.contentMode = UIViewContentModeScaleAspectFit;
+            iv.image = im;
+            [self.sess stopRunning];
+            [[self.view.layer.sublayers lastObject] removeFromSuperlayer];
+            [self.view addSubview: iv];
+        });
+    };
+    [self.snapper captureStillImageAsynchronouslyFromConnection:vc 
+                                              completionHandler:h];
 }
 
 @end
