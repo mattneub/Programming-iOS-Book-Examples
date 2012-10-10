a5b01d2d99b2e3c0dbb206aa7cf6e821d458c5cc
Matt Neuburg <matt@tidbits.com>
2012-10-10 16:54:23 -0700

    use constraints to center subview

--- ch15p386contentMode/p333p342contentMode/AppDelegate.m
+++ ch15p386contentMode/p333p342contentMode/AppDelegate.m
@@ -19,16 +19,43 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = [UIViewController new];
     
-    MyView* mv = [[MyView alloc] 
-                  initWithFrame: CGRectMake(0, 0, self.window.bounds.size.width - 50, 150)];
-    mv.center = self.window.center;
+//    MyView* mv = [[MyView alloc] 
+//                  initWithFrame: CGRectMake(0, 0, self.window.bounds.size.width - 50, 150)];
+//    mv.center = self.window.center;
+    MyView* mv = [MyView new];
     [self.window.rootViewController.view addSubview: mv];
     mv.opaque = NO;
     mv.tag = 111; // so I can get a reference to this view later
+    
+    mv.translatesAutoresizingMaskIntoConstraints = NO;
+    NSArray* cons;
+    NSLayoutConstraint* con;
+    cons = [NSLayoutConstraint
+            constraintsWithVisualFormat:@"H:|-25-[v]-25-|"
+            options:0 metrics:nil
+            views:@{@"v":mv}];
+    [mv.superview addConstraints:cons];
+    cons = [NSLayoutConstraint
+            constraintsWithVisualFormat:@"V:[v(150)]"
+            options:0 metrics:nil
+            views:@{@"v":mv}];
+    [mv addConstraints:cons];
+    // I don't think we can say this in the visual format language
+    con = [NSLayoutConstraint
+           constraintWithItem:mv
+           attribute:NSLayoutAttributeCenterY
+           relatedBy:NSLayoutRelationEqual
+           toItem:mv.superview
+           attribute:NSLayoutAttributeCenterY
+           multiplier:1 constant:0];
+    [mv.superview addConstraint:con];
 
-    [self performSelector:@selector(resize:) withObject:nil afterDelay:0.1];
+
+    [self performSelector:@selector(resize:) withObject:nil afterDelay:1];
     // NOTE: this is another way of doing the same thing, i.e. wait until after redraw moment
     // [CATransaction setCompletionBlock:^{[self resize:nil];}];
+    
+    // NOTE: another way to see the effect would be to rotate the interface, thus changing our width
     
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
