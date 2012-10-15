0c25cb3799b526876046e8eac68c2cb3c3a967ef
Matt Neuburg <matt@tidbits.com>
2012-10-15 12:20:53 -0700

    one more ch19 orientation examples

---
+++ ch19p520orientationsPermitted3/SupportedOrientationsTest/AppDelegate.m
@@ -0,0 +1,27 @@
+
+
+#import "AppDelegate.h"
+
+#import "ViewController.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+
+-(NSUInteger)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
+    return UIInterfaceOrientationMaskLandscape;
+}
+
+// remember to return a mask, not an orientation!
+
+
+@end
