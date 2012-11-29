382265670f703f5dde8a7cf82d9be8a69b13024e
Matt Neuburg <matt@tidbits.com>
2012-11-29 10:52:15 -0800

    new example to show table view automatically resizing for keyboard

---
+++ ch23p679textFieldInTableView/tableViewAndKeyboardTest/MasterViewController.m
@@ -0,0 +1,39 @@
+
+
+#import "MasterViewController.h"
+
+
+@interface MasterViewController () <UITextFieldDelegate>
+@end
+
+@implementation MasterViewController
+
+
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    NSString *s = [NSString stringWithFormat:@"%i", indexPath.row];
+    [(UITextField*)[cell.contentView subviews][0] setPlaceholder:s];
+    return cell;
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField endEditing:YES];
+    return NO;
+}
+
+
+@end
