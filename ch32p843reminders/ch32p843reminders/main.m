9345f7e9a5641fd265ef502c8b25bd03df268315
Matt Neuburg <matt@tidbits.com>
2012-12-04 15:23:18 -0800

    added reminders example

---
+++ ch32p843reminders/ch32p843reminders/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ch32p843reminders
+//
+//  Created by Matt Neuburg on 12/3/12.
+//  Copyright (c) 2012 Matt Neuburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
