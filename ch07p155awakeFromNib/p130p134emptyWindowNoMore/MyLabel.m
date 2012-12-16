9878eaa4db7f26d55add521a02d64eef777f7f96
Matt Neuburg <matt@tidbits.com>
2012-12-16 13:02:35 -0800

    underscore before ivar name

--- ch07p155awakeFromNib/p130p134emptyWindowNoMore/MyLabel.m
+++ ch07p155awakeFromNib/p130p134emptyWindowNoMore/MyLabel.m
@@ -4,13 +4,13 @@
 
 @implementation MyLabel
 {
-    int num;
+    int _num;
 }
 
 - (id) initWithCoder:(NSCoder *)aDecoder { 
     self = [super initWithCoder:aDecoder]; 
     if (self) {
-        self->num = 42; 
+        self->_num = 42; 
     }
     return self;
 }
@@ -22,7 +22,7 @@
 
 - (void) awakeFromNib { 
     [super awakeFromNib]; 
-    self.text = [NSString stringWithFormat: @"The answer is %i", self->num];
+    self.text = [NSString stringWithFormat: @"The answer is %i", self->_num];
     [self sizeToFit];
 }
 
