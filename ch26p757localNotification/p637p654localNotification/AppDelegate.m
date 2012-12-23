c4cb5c20f80615d7299a91a61338a4a5c8d28ec6
Matt Neuburg <matt@tidbits.com>
2012-12-22 19:16:41 -0800

    added some commented-out logging to determine event order

--- ch26p757localNotification/p637p654localNotification/AppDelegate.m
+++ ch26p757localNotification/p637p654localNotification/AppDelegate.m
@@ -24,14 +24,30 @@
 }
 
 - (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)n {
+//    NSLog(@"start %@", NSStringFromSelector(_cmd));
     BOOL inactive = ([[UIApplication sharedApplication] applicationState] == UIApplicationStateInactive);
     UIAlertView* av = [[UIAlertView alloc] initWithTitle:@"Hey" message:[NSString stringWithFormat:@"While %@, I received a local notification: %@", inactive ? @"inactive": @"active", n.alertBody] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
     [av show];
+//    NSLog(@"end %@", NSStringFromSelector(_cmd));
 }
 
 - (void) doAlert: (UILocalNotification*) n {
     UIAlertView* av = [[UIAlertView alloc] initWithTitle:@"Hey" message:[NSString stringWithFormat:@"I got launched by a local notification: %@", n.alertBody] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
     [av show];
 }
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+//    NSLog(@"start %@", NSStringFromSelector(_cmd));
+//    NSLog(@"end %@", NSStringFromSelector(_cmd));
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+//    NSLog(@"start %@", NSStringFromSelector(_cmd));
+//    NSLog(@"end %@", NSStringFromSelector(_cmd));
+}
+
+// logging shows that didReceive is called between willEnter and didBecome
+
 
 @end
