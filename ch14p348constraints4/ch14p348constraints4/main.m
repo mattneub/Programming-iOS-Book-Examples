948046bb47c6ee0daedc40cfc2f8adce6d39d949
Matt Neuburg <matt@tidbits.com>
2012-12-27 10:15:05 -0800

    new constraint example

---
+++ ch14p348constraints4/ch14p348constraints4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ch14p348constraints4
+//
+//  Created by Matt Neuburg on 12/27/12.
+//  Copyright (c) 2012 Matt Neuburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
