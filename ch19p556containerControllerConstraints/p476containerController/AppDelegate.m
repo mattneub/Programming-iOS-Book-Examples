ce0bb07490be55d5dda3ec579cc7dbafc25c2f92
Matt Neuburg <matt@tidbits.com>
2013-01-11 09:19:42 -0800

    new example, custom container controller positioning child view with constraints

---
+++ ch19p556containerControllerConstraints/p476containerController/AppDelegate.m
@@ -0,0 +1,21 @@
+
+
+#import "AppDelegate.h"
+
+#import "ViewController.h"
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+
+@end
