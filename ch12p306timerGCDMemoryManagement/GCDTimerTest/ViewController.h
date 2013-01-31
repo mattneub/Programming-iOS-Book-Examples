318e9db125e685127f1abf03a79986658751d62b
Matt Neuburg <matt@tidbits.com>
2013-01-31 08:53:40 -0800

    new example of GCD timer and its memory management

---
+++ ch12p306timerGCDMemoryManagement/GCDTimerTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GCDTimerTest
+//
+//  Created by Matt Neuburg on 1/29/13.
+//  Copyright (c) 2013 Matt Neuburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
