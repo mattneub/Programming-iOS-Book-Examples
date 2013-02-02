b19bfeee524ebfefee085036526ef113ffd2edae
Matt Neuburg <matt@tidbits.com>
2013-02-01 19:41:33 -0800

    expand action sheet on iPad example to show action sheet inside popover

--- ch26p754actionSheetPopover/p623p651actionSheetPopover/AppDelegate.h
+++ ch26p754actionSheetPopover/p623p651actionSheetPopover/AppDelegate.h
@@ -2,8 +2,11 @@
 
 #import <UIKit/UIKit.h>
 
+#define APPDEL ((AppDelegate*)[[UIApplication sharedApplication] delegate])
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIPopoverController* currentPop;
 
 @end
