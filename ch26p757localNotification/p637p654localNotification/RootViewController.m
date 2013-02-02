54f11ce40f0e7d1e479cdace39d9b1eee4c074b4
Matt Neuburg <matt@tidbits.com>
2013-02-01 19:40:34 -0800

    minor tweaks

--- ch26p757localNotification/p637p654localNotification/RootViewController.m
+++ ch26p757localNotification/p637p654localNotification/RootViewController.m
@@ -4,10 +4,10 @@
 @implementation RootViewController
 
 - (IBAction)doButton:(id)sender {
-    UILocalNotification* ln = [[UILocalNotification alloc] init];
+    UILocalNotification* ln = [UILocalNotification new];
     ln.alertBody = @"Time for another cup of coffee!";
     //ln.alertAction = @"What";
-    ln.hasAction = YES;
+    ln.hasAction = YES; // try NO; has no effect
     ln.fireDate = [NSDate dateWithTimeIntervalSinceNow:15];
     ln.soundName = UILocalNotificationDefaultSoundName;
     [[UIApplication sharedApplication] scheduleLocalNotification:ln];
