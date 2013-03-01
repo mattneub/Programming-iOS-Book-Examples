9ce2ec4675cbd2f7b38e0731ab9f9fca519aec6f
Matt Neuburg <matt@tidbits.com>
2013-03-01 07:57:49 -0800

    forgot to clean up code on this example

--- ch14p348constraints5/ch14p348constraints5/MyView.h
+++ ch14p348constraints5/ch14p348constraints5/MyView.h
@@ -1,10 +1,4 @@
-//
-//  MyView.h
-//  ch14p348constraints5
-//
-//  Created by Matt Neuburg on 12/27/12.
-//  Copyright (c) 2012 Matt Neuburg. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
--- ch14p348constraints5/ch14p348constraints5/ViewController.m
+++ ch14p348constraints5/ch14p348constraints5/ViewController.m
@@ -1,10 +1,3 @@
-//
-//  ViewController.m
-//  ch14p348constraints5
-//
-//  Created by Matt Neuburg on 12/27/12.
-//  Copyright (c) 2012 Matt Neuburg. All rights reserved.
-//
 
 #import "ViewController.h"
 
