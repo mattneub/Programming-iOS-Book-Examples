8f161da2f97018b7b0b4190842bdd9e2f870d46f
Matt Neuburg <matt@tidbits.com>
2013-10-22 09:51:42 -0700

    make last chapter 23 example tappable as in previous editions

---
+++ ch23p815attributedStringDrawing3/ch23p815attributedStringDrawing3/MyLayoutManager.m
@@ -0,0 +1,32 @@
+
+
+#import "MyLayoutManager.h"
+
+@interface MyLayoutManager ()
+@end
+
+@implementation MyLayoutManager
+
+-(void)drawBackgroundForGlyphRange:(NSRange)glyphsToShow atPoint:(CGPoint)origin {
+    [super drawBackgroundForGlyphRange:glyphsToShow atPoint:origin];
+    if (!self.wordRange.length)
+        return;
+    NSRange range = [self glyphRangeForCharacterRange:self.wordRange actualCharacterRange:nil];
+    range = NSIntersectionRange(glyphsToShow, range);
+    if (!range.length)
+        return;
+    
+    NSTextContainer* tc = [self textContainerForGlyphAtIndex:range.location effectiveRange:nil];
+    CGRect r = [self boundingRectForGlyphRange:range inTextContainer:tc];
+    r.origin.x += origin.x;
+    r.origin.y += origin.y;
+    r = CGRectInset(r, -2, 0);
+    CGContextRef c = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(c);
+    CGContextSetStrokeColorWithColor(c, [UIColor blackColor].CGColor);
+    CGContextSetLineWidth(c, 1.0);
+    CGContextStrokeRect(c, r);
+    CGContextRestoreGState(c);
+}
+
+@end
--- ch23p815attributedStringDrawing3/ch23p815attributedStringDrawing3/StyledText.m
+++ ch23p815attributedStringDrawing3/ch23p815attributedStringDrawing3/StyledText.m
@@ -1,10 +1,17 @@
 
 
 #import "StyledText.h"
+#import "MyLayoutManager.h"
 @import CoreText;
 
 @interface StyledText()
 @property (nonatomic, copy) NSAttributedString* text;
+@property (nonatomic, strong) NSLayoutManager* lm;
+@property (nonatomic, strong) NSTextContainer* tc;
+@property (nonatomic, strong) NSTextContainer* tc2;
+@property (nonatomic, strong) NSTextStorage* ts;
+@property CGRect r1;
+@property CGRect r2;
 @end
 
 @implementation StyledText
@@ -28,14 +35,20 @@
     para.alignment = NSTextAlignmentCenter;
     [mas addAttribute:NSParagraphStyleAttributeName value:para range:NSMakeRange(0,mas.length)];
     self.text = mas;
+    
+    UITapGestureRecognizer* tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+    [self addGestureRecognizer:tap];
 }
 
-- (void) drawRect:(CGRect)rect {
+-(void)layoutSubviews {
+    NSLog(@"%@", @"layout");
+    [super layoutSubviews];
     CGRect r1 = self.bounds;
+    r1.origin.y += 2; // a little top space
     r1.size.width /= 2.0; // column 1
     CGRect r2 = r1;
     r2.origin.x += r2.size.width; // column 2
-    NSLayoutManager* lm = [NSLayoutManager new];
+    NSLayoutManager* lm = [MyLayoutManager new];
     NSTextStorage* ts =
     [[NSTextStorage alloc] initWithAttributedString:self.text];
     [ts addLayoutManager:lm];
@@ -43,14 +56,62 @@
     [lm addTextContainer:tc];
     NSTextContainer* tc2 = [[NSTextContainer alloc] initWithSize:r2.size];
     [lm addTextContainer:tc2];
-    NSRange range1 = [lm glyphRangeForTextContainer:tc];
-    [lm drawBackgroundForGlyphRange:range1 atPoint:r1.origin];
-    [lm drawGlyphsForGlyphRange:range1 atPoint:r1.origin];
-    NSRange range2 = [lm glyphRangeForTextContainer:tc2];
-    [lm drawBackgroundForGlyphRange:range2 atPoint:r2.origin];
-    [lm drawGlyphsForGlyphRange:range2 atPoint:r2.origin];
+    
+    self.lm = lm;
+    self.ts = ts;
+    self.tc = tc;
+    self.tc2 = tc2;
+    self.r1 = r1;
+    self.r2 = r2;
 
+}
 
+- (void) drawRect:(CGRect)rect {
+    NSRange range1 = [self.lm glyphRangeForTextContainer:self.tc];
+    [self.lm drawBackgroundForGlyphRange:range1 atPoint:self.r1.origin];
+    [self.lm drawGlyphsForGlyphRange:range1 atPoint:self.r1.origin];
+    NSRange range2 = [self.lm glyphRangeForTextContainer:self.tc2];
+    [self.lm drawBackgroundForGlyphRange:range2 atPoint:self.r2.origin];
+    [self.lm drawGlyphsForGlyphRange:range2 atPoint:self.r2.origin];
+}
+
+- (void) tap: (UIGestureRecognizer*) g {
+    // which column is it in?
+    CGPoint p = [g locationInView:self];
+    NSTextContainer* tc = self.tc;
+    if (!CGRectContainsPoint(self.r1, p)) {
+        tc = self.tc2;
+        p.x -= self.r1.size.width;
+    }
+    CGFloat f;
+    NSUInteger ix = [self.lm glyphIndexForPoint:p inTextContainer:tc fractionOfDistanceThroughGlyph:&f];
+    NSRange glyphRange;
+    [self.lm lineFragmentRectForGlyphAtIndex:ix effectiveRange:&glyphRange];
+    // NSLog(@"ix:%d f:%f glphRange:%@", ix, f, NSStringFromRange(glyphRange));
+    // if ix is the first glyph of the line and f is 0...
+    // or ix is the last glyph of the line and f is 1...
+    // you missed the word entirely
+    if (ix == glyphRange.location && f == 0.0)
+        return;
+    if (ix == glyphRange.location + glyphRange.length - 1 && f == 1.0)
+        return;
+    // eliminate control character glyphs at end
+    while (NSGlyphPropertyControlCharacter && [self.lm propertyForGlyphAtIndex:glyphRange.location + glyphRange.length - 1])
+        glyphRange.length -= 1;
+    NSRange characterRange = [self.lm characterRangeForGlyphRange:glyphRange actualGlyphRange:nil];
+    NSString* s = [self.text.string substringWithRange:characterRange]; // state name
+    NSLog(@"you tapped %@", s);
+    MyLayoutManager* lm = (MyLayoutManager*)self.lm;
+    lm.wordRange = characterRange;
+    [self setNeedsDisplay];
+    [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
+    double delayInSeconds = 0.3;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        lm.wordRange = NSMakeRange(0, 0);
+        [self setNeedsDisplay];
+        [[UIApplication sharedApplication] endIgnoringInteractionEvents];
+    });
 }
 
 
