2f67f40f8051d832e1989ab56f9e5204d32b31cb
Matt Neuburg <matt@tidbits.com>
2013-10-25 12:45:41 -0700

    first chapter 25 example (previously I meant 24)

---
+++ ch25p837timeConsumingNavigation/ch25p837timeConsumingNavigation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ch25p837timeConsumingNavigation
+//
+//  Created by Matt Neuburg on 10/25/13.
+//  Copyright (c) 2013 Matt Neuburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ch25p837timeConsumingNavigation/ch25p837timeConsumingNavigation/ViewController.m
@@ -0,0 +1,20 @@
+
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+
+@end
