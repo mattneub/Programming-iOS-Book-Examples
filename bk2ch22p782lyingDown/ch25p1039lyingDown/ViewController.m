f0828a12634b219e5decc6562b7b1e618ad28da2
Matt Neuburg <matt@tidbits.com>
2014-04-24 12:09:48 -0700

    illustrate use of queue callbacks as alternative to polling motion manager

--- bk2ch22p782lyingDown/ch25p1039lyingDown/ViewController.m
+++ bk2ch22p782lyingDown/ch25p1039lyingDown/ViewController.m
@@ -14,22 +14,55 @@
     NSInteger _state;
 }
 
+#define which 2 // try also 2
+
+- (void) stopAccelerometer {
+    [self.timer invalidate];
+    self.timer = nil;
+    [self.motman stopAccelerometerUpdates];
+    self.label.text = @"";
+    _oldX = 0;
+    _oldY = 0;
+    _oldZ = 0;
+    _state = 0;
+}
+
 - (IBAction)doButton:(id)sender {
-    self.motman = [CMMotionManager new];
+    if (self.motman && self.motman.accelerometerActive) {
+        [self stopAccelerometer];
+        return;
+    }
+    if (!self.motman)
+        self.motman = [CMMotionManager new];
     if (!self.motman.accelerometerAvailable) {
         NSLog(@"oh well");
         return;
     }
     self.motman.accelerometerUpdateInterval = 1.0 / 30.0;
+    
+#if which == 1
+    
     [self.motman startAccelerometerUpdates];
     self.timer = [NSTimer scheduledTimerWithTimeInterval:self.motman.accelerometerUpdateInterval target:self selector:@selector(pollAccel:) userInfo:nil repeats:YES];
+    
+#elif which == 2
+    
+[self.motman startAccelerometerUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
+    // should I be using weak-strong dance here?
+    if (error) {
+        NSLog(@"%@", error);
+        [self stopAccelerometer];
+        return;
+    }
+    [self receiveAccel: accelerometerData];
+}];
+    
+#endif
 }
 
 - (void) viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
-    [self.timer invalidate];
-    self.timer = nil;
-    [self.motman stopAccelerometerUpdates];
+    [self stopAccelerometer];
 }
 
 -(void)addAcceleration:(CMAcceleration)accel {
@@ -49,6 +82,25 @@
     //    CGFloat x = acc.x;
     //    CGFloat y = acc.y;
     //    CGFloat z = acc.z;
+    CGFloat accu = 0.08;
+    if (fabs(x) < accu && fabs(y) < accu && z < -0.5) {
+        if (self->_state == -1 || self->_state == 1) {
+            self->_state = 0;
+            self.label.text = @"I'm lying on my back... ahhh...";
+        }
+    } else {
+        if (self->_state == -1 || self->_state == 0) {
+            self->_state = 1;
+            self.label.text = @"Hey, put me back down on the table!";
+        }
+    }
+}
+
+- (void) receiveAccel: (CMAccelerometerData*) acc {
+    [self addAcceleration: acc.acceleration];
+    CGFloat x = self->_oldX;
+    CGFloat y = self->_oldY;
+    CGFloat z = self->_oldZ;
     CGFloat accu = 0.08;
     if (fabs(x) < accu && fabs(y) < accu && z < -0.5) {
         if (self->_state == -1 || self->_state == 1) {
