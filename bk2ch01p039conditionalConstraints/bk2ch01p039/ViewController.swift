ac5152eb2a67dfe3ed2758361b70bbdd4d1f3d5b
Matt Neuburg <matt@tidbits.com>
2014-07-06 09:13:51 -0700

    new example showing conditional constraints configured in the nib

---
+++ bk2ch01p039conditionalConstraints/bk2ch01p039/ViewController.swift
@@ -0,0 +1,14 @@
+
+
+import UIKit
+
+// demonstrating a new Xcode 6 feature: 
+// conditional constraints (using size classes) in Interface Builder
+// we have our base set of constraints on iPad
+// another set (based on that) for iPhone portrait
+// another set (based on that) iPhone landscape
+
+class ViewController: UIViewController {
+
+
+}
