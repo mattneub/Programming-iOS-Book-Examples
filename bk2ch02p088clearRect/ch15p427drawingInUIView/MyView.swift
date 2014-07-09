ddac81224f46eaf5c589a01f9eb3212dd6bb229a
Matt Neuburg <matt@tidbits.com>
2014-07-09 15:28:13 -0700

    finished translating bk 2 ch 2 examples into Swift

---
+++ bk2ch02p088clearRect/ch15p427drawingInUIView/MyView.swift
@@ -0,0 +1,26 @@
+
+import UIKit
+
+class MyView : UIView {
+    
+    init (frame:CGRect) {
+        super.init(frame:frame)
+        self.opaque = false
+        self.backgroundColor = UIColor.redColor()
+        // clearRect will cause a black square
+        self.backgroundColor = self.backgroundColor.colorWithAlphaComponent(1)
+        // but uncomment the next line: clearRect will cause a clear square!
+        // self.backgroundColor = self.backgroundColor.colorWithAlphaComponent(0.99)
+        
+        println("Layer opaque: \(self.layer.opaque)")
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let con = UIGraphicsGetCurrentContext()
+        CGContextSetFillColorWithColor(con, UIColor.blueColor().CGColor)
+        CGContextFillRect(con, rect)
+        CGContextClearRect(con, CGRectMake(0,0,30,30))
+
+    }
+    
+}
