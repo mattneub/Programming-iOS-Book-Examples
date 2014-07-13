184393f77ed1ea4fdf0be9c9b198b37c6ede09ab
Matt Neuburg <matt@tidbits.com>
2014-07-13 11:47:41 -0700

    still continuing to convert Bk 2 chapter 5 examples to Swift

--- bk2ch05p211compassTappable/ch17p490layerAnimation/ViewController.m
+++ bk2ch05p211compassTappable/ch17p490layerAnimation/ViewController.m
@@ -1,18 +1,11 @@
 
 
 #import "ViewController.h"
-#import "CompassView.h"
-#import "CompassLayer.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet CompassView *compassView;
 
 @end
 
 @implementation ViewController
-
-
-    
-
 
 @end
---
+++ bk2ch05p213hitTestingDuringAnimation/ch18p555hitTestingDuringAnimation/MyView.swift
@@ -0,0 +1,40 @@
+
+
+import UIKit
+import QuartzCore
+
+class MyView : UIView {
+    @IBOutlet var v : UIView // the animated subview
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let t = UITapGestureRecognizer(target: self, action: "tap:")
+        self.v.addGestureRecognizer(t)
+        t.cancelsTouchesInView = false
+        // uncomment next line to see how button, even if tappable, "swallows the touch" while animating
+        // self.v.removeGestureRecognizer(t)
+    }
+    
+    func tap(g:UIGestureRecognizer!) {
+        println("tap! (gesture recognizer)")
+    }
+    
+    override func hitTest(point: CGPoint, withEvent event: UIEvent!) -> UIView! {
+        // uncomment this next line to see what it's like without our munged hit-testing
+        // return super.hitTest(point, withEvent: event)
+        // v is the animated subview
+        if let lay = self.v.layer.presentationLayer() as? CALayer {
+            if let hitLayer = lay.hitTest(point) {
+                if (hitLayer == lay || hitLayer.superlayer == lay) {
+                    return self.v
+                }
+            }
+        }
+        let hitView = super.hitTest(point, withEvent:event)
+        if (hitView == self.v) {
+            return self
+        }
+        return hitView
+    }
+    
+}
