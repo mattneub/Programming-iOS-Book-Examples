f7490e7b29fc2632bdbac4f1f000620c186d7e49
Matt Neuburg <matt@tidbits.com>
2014-07-15 11:50:03 -0700

    still continuing to convert Bk 2 chapter 6 examples to Swift

---
+++ bk2ch06p257rotationChangesInterface3/bk2ch06p257rotationChangesInterface3/ViewController.swift
@@ -0,0 +1,17 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    // hey, where did all the code go? 
+    // Surprise, iOS 8 has conditional constraints in the nib!
+    // solves the whole problem, badda bing badda boom
+    
+    // made slightly more complicated by:
+    // (1) inability to express certain relationships in IB
+    // (2) fact that status bar will be hidden in landscape only by default...
+    // ...thus shifting the top guide which is the only thing we can pin to in IB
+
+}
+
