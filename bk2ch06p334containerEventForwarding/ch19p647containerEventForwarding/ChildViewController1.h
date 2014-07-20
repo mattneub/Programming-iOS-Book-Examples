2d41ce07f2ab16b02e769ed751cc013bc36da8e8
Matt Neuburg <matt@tidbits.com>
2014-07-19 19:41:57 -0700

    still continuing with swift translation of chapter 6 examples

--- bk2ch06p334containerEventForwarding/ch19p647containerEventForwarding/ChildViewController1.h
+++
@@ -1,7 +0,0 @@
-
-
-#import <UIKit/UIKit.h>
-
-@interface ChildViewController1 : UIViewController
-
-@end
--- bk2ch06p334containerEventForwarding/ch19p647containerEventForwarding/ChildViewController2.h
+++
@@ -1,7 +0,0 @@
-
-
-#import <UIKit/UIKit.h>
-
-@interface ChildViewController2 : UIViewController
-
-@end
