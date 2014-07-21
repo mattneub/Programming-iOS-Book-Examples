34e4fd2527dfbc8babcd3388c93c357da62e6a94
Matt Neuburg <matt@tidbits.com>
2014-07-21 09:07:20 -0700

    converted first three scroll view examples (chapter 7) to swift, added example showing workaround for iOS 8 bug (sometimes I amaze even myself)

---
+++ bk2ch07p360scrollViewInCode/ch20p570scrollViewInCode/ViewController.swift
@@ -0,0 +1,35 @@
+
+
+import UIKit
+
+class ViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let sv = UIScrollView(frame: self.view.bounds)
+        sv.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        self.view.addSubview(sv)
+        sv.backgroundColor = UIColor.whiteColor()
+        var y : CGFloat = 10
+        for i in 0 ..< 30 {
+            let lab = UILabel()
+            lab.text = "This is label \(i+1)"
+            lab.sizeToFit()
+            lab.frame.origin = CGPointMake(10,y)
+            sv.addSubview(lab)
+            y += lab.bounds.size.height + 10
+            
+            // uncomment
+//            lab.frame.size.width = self.view.bounds.size.width - 20
+//            lab.backgroundColor = UIColor.redColor() // make label bounds visible
+//            lab.autoresizingMask = .FlexibleWidth
+            
+        }
+        var sz = sv.bounds.size
+        sz.height = y
+        sv.contentSize = sz // This is the crucial line
+
+    }
+    
+}
---
+++ bk2ch07p362scrollViewInCode2/ch20p670scrollViewInCode2/ViewController.swift
@@ -0,0 +1,62 @@
+
+
+import UIKit
+
+class ViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let sv = UIScrollView()
+        // sv.alwaysBounceHorizontal = true
+        sv.backgroundColor = UIColor.whiteColor()
+        sv.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.view.addSubview(sv)
+        self.view.addConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "H:|[sv]|",
+                options:nil, metrics:nil,
+                views:["sv":sv]))
+        self.view.addConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "V:|[sv]|",
+                options:nil, metrics:nil,
+                views:["sv":sv]))
+        var previousLab : UILabel? = nil
+        for i in 0 ..< 30 {
+            let lab = UILabel()
+            // lab.backgroundColor = UIColor.redColor()
+            lab.setTranslatesAutoresizingMaskIntoConstraints(false)
+            lab.text = "This is label \(i+1)"
+            sv.addSubview(lab)
+            sv.addConstraints(
+                NSLayoutConstraint.constraintsWithVisualFormat(
+                    "H:|-(10)-[lab]",
+                    options:nil, metrics:nil,
+                    views:["lab":lab]))
+            if previousLab == nil { // first one, pin to top
+                sv.addConstraints(
+                    NSLayoutConstraint.constraintsWithVisualFormat(
+                        "V:|-(10)-[lab]",
+                        options:nil, metrics:nil,
+                        views:["lab":lab]))
+            } else { // all others, pin to previous
+                sv.addConstraints(
+                    NSLayoutConstraint.constraintsWithVisualFormat(
+                        "V:[prev]-(10)-[lab]",
+                        options:nil, metrics:nil,
+                        views:["lab":lab, "prev":previousLab!]))
+            }
+            previousLab = lab
+        }
+        
+        // last one, pin to bottom, this dictates content size height!
+        sv.addConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "V:[lab]-(10)-|",
+                options:nil, metrics:nil,
+                views:["lab":previousLab!]))
+        
+    }
+    
+}
