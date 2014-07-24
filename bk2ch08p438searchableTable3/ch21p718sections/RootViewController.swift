a69a976be69101bf50695f2046adab23ff6c0267
Matt Neuburg <matt@tidbits.com>
2014-07-24 16:37:22 -0700

    another groovy UISearchController example, got rid of old unnecessary search example

---
+++ bk2ch08p438searchableTable3/ch21p718sections/RootViewController.swift
@@ -0,0 +1,176 @@
+
+
+import UIKit
+
+class RootViewController : UITableViewController, UISearchBarDelegate {
+    var sectionNames = [String]()
+    var sectionData = [[String]]()
+    var originalSectionNames = [String]()
+    var originalSectionData = [[String]]()
+    var searcher = UISearchController()
+    var searching = false
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
+        let s = NSString(contentsOfFile: NSBundle.mainBundle().pathForResource("states", ofType: "txt"), encoding: NSUTF8StringEncoding, error: nil)
+        let states = s.componentsSeparatedByString("\n") as [String]
+        var previous = ""
+        for aState in states {
+            // get the first letter
+            let c = (aState as NSString).substringWithRange(NSMakeRange(0,1))
+            // only add a letter to sectionNames when it's a different letter
+            if c != previous {
+                previous = c
+                self.sectionNames += c.uppercaseString
+                // and in that case also add new subarray to our array of subarrays
+                self.sectionData += [String]()
+            }
+            // in Swift, an array inside an array can't be mutable
+            // so pull it out, modify it, put it back
+            var arr = sectionData.removeLast()
+            arr += aState
+            sectionData += arr
+        }
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        self.tableView.registerClass(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "Header")
+        
+        self.tableView.sectionIndexColor = UIColor.whiteColor()
+        self.tableView.sectionIndexBackgroundColor = UIColor.redColor()
+        // self.tableView.sectionIndexTrackingBackgroundColor = UIColor.blueColor()
+        self.tableView.backgroundColor = UIColor.yellowColor()
+        
+        // in this version, we take the total opposite approach:
+        // we don't present any extra view at all!
+        // we already have a table, so why not just filter the very same table?
+        // to do so, pass nil as the search results controller,
+        // and tell the search controller not to insert a dimming view
+        
+        // keep copies of the original data
+        self.originalSectionData = self.sectionData
+        self.originalSectionNames = self.sectionNames
+        let searcher = UISearchController(searchResultsController:nil)
+        self.searcher = searcher
+        searcher.dimsBackgroundDuringPresentation = false
+        searcher.searchResultsUpdater = self
+        searcher.delegate = self
+        // put the search controller's search bar into the interface
+        let b = searcher.searchBar
+        b.sizeToFit() // crucial, trust me on this one
+        b.autocapitalizationType = .None
+        self.tableView.tableHeaderView = b
+        self.tableView.reloadData()
+        self.tableView.scrollToRowAtIndexPath(
+            NSIndexPath(forRow: 0, inSection: 0),
+            atScrollPosition:.Top, animated:false)
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        return self.sectionNames.count
+    }
+    
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return self.sectionData[section].count
+    }
+    
+    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        let s = self.sectionData[indexPath.section][indexPath.row]
+        cell.textLabel.text = s
+        
+        // this part is not in the book, it's just for fun
+        var stateName = s
+        stateName = stateName.lowercaseString
+        stateName = stateName.stringByReplacingOccurrencesOfString(" ", withString:"")
+        stateName = "flag_\(stateName).gif"
+        let im = UIImage(named: stateName)
+        cell.imageView.image = im
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView!, viewForHeaderInSection section: Int) -> UIView! {
+        let h = tableView.dequeueReusableHeaderFooterViewWithIdentifier("Header") as UITableViewHeaderFooterView
+        if h.tintColor != UIColor.redColor() {
+            h.tintColor = UIColor.redColor() // invisible marker, tee-hee
+            h.backgroundView = UIView()
+            h.backgroundView.backgroundColor = UIColor.blackColor()
+            let lab = UILabel()
+            lab.tag = 1
+            lab.font = UIFont(name:"Georgia-Bold", size:22)
+            lab.textColor = UIColor.greenColor()
+            lab.backgroundColor = UIColor.clearColor()
+            h.contentView.addSubview(lab)
+            let v = UIImageView()
+            v.tag = 2
+            v.backgroundColor = UIColor.blackColor()
+            v.image = UIImage(named:"us_flag_small.gif")
+            h.contentView.addSubview(v)
+            lab.setTranslatesAutoresizingMaskIntoConstraints(false)
+            v.setTranslatesAutoresizingMaskIntoConstraints(false)
+            h.contentView.addConstraints(
+                NSLayoutConstraint.constraintsWithVisualFormat("H:|-5-[lab(25)]-10-[v(40)]",
+                    options:nil, metrics:nil, views:["v":v, "lab":lab]))
+            h.contentView.addConstraints(
+                NSLayoutConstraint.constraintsWithVisualFormat("V:|[v]|",
+                    options:nil, metrics:nil, views:["v":v]))
+            h.contentView.addConstraints(
+                NSLayoutConstraint.constraintsWithVisualFormat("V:|[lab]|",
+                    options:nil, metrics:nil, views:["lab":lab]))
+        }
+        let lab = h.contentView.viewWithTag(1) as UILabel
+        lab.text = self.sectionNames[section]
+        return h
+    }
+    
+    // much nicer without section index during search
+    override func sectionIndexTitlesForTableView(tableView: UITableView!) -> [AnyObject]! {
+        return self.searching ? nil : self.sectionNames
+    }
+}
+
+extension RootViewController : UISearchControllerDelegate {
+    // flag for whoever needs it (in this case, sectionIndexTitles...)
+    func willPresentSearchController(searchController: UISearchController!) {
+        self.searching = true
+    }
+    func willDismissSearchController(searchController: UISearchController!) {
+        self.searching = false
+    }
+}
+
+extension RootViewController : UISearchResultsUpdating {
+    func updateSearchResultsForSearchController(searchController: UISearchController!) {
+        let sb = searchController.searchBar
+        let target = sb.text
+        if target == "" {
+            self.sectionNames = self.originalSectionNames
+            self.sectionData = self.originalSectionData
+            self.tableView.reloadData()
+            return
+        }
+        // we have a target string
+        self.sectionData = self.originalSectionData.map {
+            (sec:[String]) -> [String] in
+            let newsec = sec.filter {
+                s in
+                let options = NSStringCompareOptions.CaseInsensitiveSearch
+                let found = (s as NSString).rangeOfString(target, options: options).length
+                return (found != 0)
+            }
+            return newsec
+            }.filter {$0.count > 0} // is Swift cool or what?
+        self.sectionNames = self.sectionData.map {
+            (sec:[String]) -> String in
+            //return (sec[0] as NSString).substringWithRange(NSMakeRange(0,1))
+            //I don't know, it's either that or this:
+            let s = sec[0]
+            let arr = Array(s)
+            return String(arr[0])
+            }
+        self.tableView.reloadData()
+    }
+}
+
