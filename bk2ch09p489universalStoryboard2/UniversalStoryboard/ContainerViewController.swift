0fdf9835daa835f64559337dc1a500a3fb351c32
Matt Neuburg <matt@tidbits.com>
2014-07-30 19:12:00 -0700

    added option to collapse in portrait but expand in landscape

--- bk2ch09p489universalStoryboard2/UniversalStoryboard/ContainerViewController.swift
+++ bk2ch09p489universalStoryboard2/UniversalStoryboard/ContainerViewController.swift
@@ -13,6 +13,13 @@ class ContainerViewController : UIViewController {
     @IBOutlet var topConstraint: NSLayoutConstraint!
     var didInitialSetup = false
     
+    override func addChildViewController(childController: UIViewController!) {
+        super.addChildViewController(childController)
+        if let svc = self.childViewControllers[0] as? UISplitViewController {
+            svc.delegate = self // need to do this as early as humanly possible
+        }
+    }
+    
     // unfortunately, IB will not let us make a constraint to the top of the superview
     // it insists on making the constraint to the bottom of the top layout guide
     // so we take control of the situation by swapping out constraints in code at launch
@@ -28,17 +35,13 @@ class ContainerViewController : UIViewController {
     // as in comedy, timing is everything
     // this setup needs to be postponed to our initial layout (viewDidLoad is too soon)
     
+    let which = 1 // try 2, it's even more interesting
+    
     override func viewWillLayoutSubviews() {
         if !self.didInitialSetup {
             self.didInitialSetup = true
             self.view.backgroundColor = UIColor.greenColor()
             let svc = self.childViewControllers[0] as UISplitViewController
-            let traits = UITraitCollection(traitsFromCollections: [
-                UITraitCollection(horizontalSizeClass: .Regular),
-                UITraitCollection(verticalSizeClass: .Regular)
-                ])
-            self.setOverrideTraitCollection(traits, forChildViewController: svc)
-            
             svc.preferredDisplayMode = .AllVisible
             // if not collapsed, always side by side
             // if you insert the display mode button, it gives the option to hide the master column
@@ -46,8 +49,47 @@ class ContainerViewController : UIViewController {
             
             svc.preferredPrimaryColumnWidthFraction = 0.5 // this works only if also give permission...
             svc.maximumPrimaryColumnWidth = 500 // ...by setting this to a large enough value
+            
+            // this is why we are here: we can prevent collapse entirely
+            if which == 1 {
+                let traits = UITraitCollection(traitsFromCollections: [
+                    UITraitCollection(horizontalSizeClass: .Regular)
+                    ])
+                self.setOverrideTraitCollection(traits, forChildViewController: svc)
+            }
+
         }
     }
     
+    // another variant (see Apple's AdaptivePhotos example):
+    // don't override traits on launch (so, portrait, collapsed)
+    // but do override just in case we rotate to landscape
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator!) {
+        let svc = self.childViewControllers[0] as UISplitViewController
+        if which == 2 {
+            if size.width > 320 {
+                // landscape
+                let traits = UITraitCollection(traitsFromCollections: [
+                    UITraitCollection(horizontalSizeClass: .Regular)
+                    ])
+                self.setOverrideTraitCollection(traits, forChildViewController: svc)
+            } else {
+                self.setOverrideTraitCollection(nil, forChildViewController: svc)
+            }
+        }
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+    }
+}
+
+extension ContainerViewController : UISplitViewControllerDelegate {
+    // as in the template, we must take action to prevent the detail from being pushed and shown
+    // in collapsed mode
+    
+    func splitViewController(svc: UISplitViewController!,
+        collapseSecondaryViewController vc2: UIViewController!,
+        ontoPrimaryViewController vc1: UIViewController!) -> Bool {
+            return true
+    }
+
     
 }
