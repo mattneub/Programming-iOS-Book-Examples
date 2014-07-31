0aa72eb32418ad7a51130a09ae9b8fb44d4d1c32
Matt Neuburg <matt@tidbits.com>
2014-07-31 11:59:26 -0700

    starting to convert chapter 10 examples, dynamic type; new example showing that it is automatic in a table (but not elsewhere, alas)

---
+++ bk2ch10p495dynamicType/ch23p670font1dynamicType/ViewController.swift
@@ -0,0 +1,27 @@
+
+
+import UIKit
+
+class ViewController : UIViewController {
+    
+    /*
+    The problem is that dynamic type is not actually dynamic!
+    Updating the interface is up to you.
+    
+    
+*/
+    
+    @IBOutlet var lab : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "doDynamicType:", name: UIContentSizeCategoryDidChangeNotification, object: nil)
+    }
+    
+    func doDynamicType(n:NSNotification) {
+        let style = self.lab.font.fontDescriptor().objectForKey(UIFontDescriptorTextStyleAttribute) as String
+        self.lab.font = UIFont.preferredFontForTextStyle(style)
+    }
+    
+    
+}
