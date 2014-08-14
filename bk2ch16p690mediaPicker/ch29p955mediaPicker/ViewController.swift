119adcb7cdfe28b628ac2505e9669416e8011372
Matt Neuburg <matt@tidbits.com>
2014-08-13 20:03:15 -0700

    finished converting chapter 16 examples to Swift

---
+++ bk2ch16p690mediaPicker/ch29p955mediaPicker/ViewController.swift
@@ -0,0 +1,48 @@
+
+
+import UIKit
+import MediaPlayer
+
+// not fully tested on iPad yet
+
+class ViewController: UIViewController {
+    
+    @IBAction func doGo (sender:AnyObject!) {
+        self.presentPicker(sender)
+    }
+    
+    func presentPicker (sender:AnyObject) {
+        // universal app is so incredibly easy in iOS 8!
+        let picker = MPMediaPickerController(mediaTypes:.AnyAudio) // new in iOS 8, need to specify
+        picker.delegate = self
+        picker.modalPresentationStyle = .Popover // or comment this out for fullscreen on both
+        self.presentViewController(picker, animated: true, completion: nil)
+        if let pop = picker.popoverPresentationController {
+            if pop.adaptivePresentationStyle() == .Popover {
+                pop.barButtonItem = sender as UIBarButtonItem
+            }
+        }
+    }
+}
+
+extension ViewController : MPMediaPickerControllerDelegate {
+    // must implement these, as there is no automatic dismissal
+    
+    func mediaPicker(mediaPicker: MPMediaPickerController!, didPickMediaItems mediaItemCollection: MPMediaItemCollection!) {
+        let player = MPMusicPlayerController.applicationMusicPlayer()
+        player.setQueueWithItemCollection(mediaItemCollection)
+        player.play()
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func mediaPickerDidCancel(mediaPicker: MPMediaPickerController!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
+
+extension ViewController : UIBarPositioningDelegate {
+    func positionForBar(bar: UIBarPositioning!) -> UIBarPosition {
+        return .TopAttached
+    }
+}
