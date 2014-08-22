6eb7d7f57e8969563b97a7c15802651c5532cea9
Matt Neuburg <matt@tidbits.com>
2014-08-22 10:08:45 -0700

    demonstrate new iOS 8 way of passing CIImage thru a CIFilter

--- bk2ch02p070filters/ch15p419filters/ViewController.swift
+++ bk2ch02p070filters/ch15p419filters/ViewController.swift
@@ -11,31 +11,34 @@ class ViewController : UIViewController {
         let moici = CIImage(image:moi)
         let moiextent = moici.extent()
         
-        let grad = CIFilter(name: "CIRadialGradient")
         let center = CIVector(x: moiextent.width/2.0, y: moiextent.height/2.0)
         
         let smallerDimension = min(moiextent.width, moiextent.height)
         let largerDimension = max(moiextent.width, moiextent.height)
         
+        // old way: form filter, set values, get output
+        let grad = CIFilter(name: "CIRadialGradient")
         grad.setValue(center, forKey:"inputCenter")
         grad.setValue(smallerDimension/2.0 * 0.85, forKey:"inputRadius0")
         grad.setValue(largerDimension/2.0, forKey:"inputRadius1")
         let gradimage = grad.outputImage
         
-        let blend = CIFilter(name: "CIBlendWithMask")
-        blend.setValue(moici, forKey: "inputImage")
-        blend.setValue(gradimage, forKey: "inputMaskImage")
+        // new iOS 8 way (only if there is an input image): turn one image into another
+        let blendimage = moici.imageByApplyingFilter(
+            "CIBlendWithMask", withInputParameters: [
+                "inputMaskImage":gradimage
+            ])
         
         // two ways to obtain final bitmap
         
         let which = 1
         switch which {
         case 1:
-            let moicg = CIContext(options: nil).createCGImage(blend.outputImage, fromRect: moiextent)
+            let moicg = CIContext(options: nil).createCGImage(blendimage, fromRect: moiextent)
             self.iv.image = UIImage(CGImage: moicg)
         case 2:
             UIGraphicsBeginImageContextWithOptions(moiextent.size, false, 0)
-            UIImage(CIImage: blend.outputImage).drawInRect(moiextent)
+            UIImage(CIImage: blendimage).drawInRect(moiextent)
             let im = UIGraphicsGetImageFromCurrentImageContext()
             UIGraphicsEndImageContext()
             self.iv.image = im
