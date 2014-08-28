4cfec1024b4004c7f3b34adba0b6fe91ecdaa9be
Matt Neuburg <matt@tidbits.com>
2014-08-28 10:04:00 -0700

    converted file handoff example to Swift, had to switch to txt instead of pdf because iOS 8 isn't currently previewing PDFs

---
+++ bk2ch23p802fileHandoff/ch36p1060fileHandoff/AppDelegate.swift
@@ -0,0 +1,72 @@
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate : UIResponder, UIApplicationDelegate {
+    var window : UIWindow?
+    
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        println("start \(__FUNCTION__)")
+        println("end \(__FUNCTION__)")
+
+        return true
+    }
+    
+    // handleOpenURL deprecated? but it does seem to work just fie
+    
+//    func application(_ application: UIApplication!,
+//        openURL url: NSURL!,
+//        sourceApplication sourceApplication: String!,
+//        annotation annotation: AnyObject!) -> Bool {
+//    }
+    
+    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
+        println("start \(__FUNCTION__)")
+        println(url)
+        
+        // new code: copy out of the inbox into the documents folder
+        // I don't know why, but for some reason Quick Look can no longer preview
+        // a document in the inbox
+        
+        var finalurl = url
+
+        let dir = url.URLByDeletingLastPathComponent?.lastPathComponent
+        if dir == "Inbox" {
+            println("inbox")
+            let fm = NSFileManager()
+            var err : NSError?
+            let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)
+            let dest = docsurl!.URLByAppendingPathComponent(url.lastPathComponent)
+            println("copying")
+            var ok = fm.copyItemAtURL(url, toURL: dest, error: &err)
+            if !ok {
+                println(err)
+            } else {
+                finalurl = dest
+                println("removing")
+                ok = fm.removeItemAtURL(url, error: &err)
+                if !ok {
+                    println(err)
+                }
+            }
+        }
+        
+        let vc = self.window?.rootViewController as ViewController
+        vc.displayPDF(finalurl)
+        println("end \(__FUNCTION__)")
+        return true
+    }
+    
+    func applicationWillEnterForeground(application: UIApplication) {
+        println("start \(__FUNCTION__)")
+        println("end \(__FUNCTION__)")
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        println("start \(__FUNCTION__)")
+        println("end \(__FUNCTION__)")
+    }
+    
+    // logging shows that handleOpenURL: comes *between* willEnter and didBecome
+
+}
---
+++ bk2ch23p802fileHandoff/ch36p1060fileHandoff/ViewController.swift
@@ -0,0 +1,115 @@
+
+
+import UIKit
+import QuickLook
+
+class ViewController: UIViewController, UIDocumentInteractionControllerDelegate, QLPreviewControllerDataSource {
+
+    @IBOutlet var wv : UIWebView!
+    var doc : NSURL!
+    var docs : [NSURL]!
+    var dic : UIDocumentInteractionController!
+    let ext = "txt"
+    
+    func displayDoc (url:NSURL) {
+        println("displayDoc: \(url)")
+        self.doc = url
+        let req = NSURLRequest(URL: url)
+        self.wv.loadRequest(req)
+    }
+    
+    func locateDoc () -> NSURL? {
+        var url : NSURL? = nil
+        let fm = NSFileManager()
+        var err : NSError?
+        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)
+        let dir = fm.enumeratorAtURL(docsurl!, includingPropertiesForKeys: nil, options: nil, errorHandler: nil)
+        while let f = dir.nextObject() as? NSURL {
+            if f.pathExtension == self.ext {
+                url = f
+                break
+            }
+        }
+        return url
+    }
+    
+    @IBAction func doDisplayDoc(sender: AnyObject) {
+        var url = self.doc
+        if url == nil {
+            url = self.locateDoc()
+        }
+        if url == nil {
+            println("no doc")
+            return
+        }
+        self.displayDoc(url)
+    }
+    
+    @IBAction func doHandOffDoc (sender:AnyObject) {
+        var url = self.doc
+        if url == nil {
+            url = self.locateDoc()
+        }
+        if url == nil {
+            println("no doc")
+            return
+        }
+        self.dic = UIDocumentInteractionController(URL:url)
+        let v = sender as UIView
+        let ok = self.dic.presentOpenInMenuFromRect(v.bounds, inView: v, animated: true)
+        if !ok {
+            println("That didn't work out")
+        }
+    }
+    
+    @IBAction func doPreview (sender:AnyObject!) {
+        var url = self.doc
+        if url == nil {
+            url = self.locateDoc()
+        }
+        if url == nil {
+            println("no doc")
+            return
+        }
+        println(url)
+        self.dic = UIDocumentInteractionController(URL:url)
+        self.dic.delegate = self
+        self.dic.presentPreviewAnimated(true)
+    }
+    
+    func documentInteractionControllerViewControllerForPreview(controller: UIDocumentInteractionController!) -> UIViewController! {
+        return self
+    }
+    
+    @IBAction func doPreviewMultipleUsingQuickLook (sender:AnyObject!) {
+        self.docs = [NSURL]()
+        let fm = NSFileManager()
+        var err : NSError?
+        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)
+        let dir = fm.enumeratorAtURL(docsurl!, includingPropertiesForKeys: nil, options: nil, errorHandler: nil)
+        while let f = dir.nextObject() as? NSURL {
+            if f.pathExtension == self.ext {
+                println(QLPreviewController.canPreviewItem(f))
+                self.docs.append(f)
+            }
+        }
+        if self.docs.count == 0 {
+            return
+        }
+        // show preview interface
+        let preview = QLPreviewController()
+        preview.dataSource = self
+        self.presentViewController(preview, animated: true, completion: nil)
+    }
+    
+    func numberOfPreviewItemsInPreviewController(controller: QLPreviewController!) -> Int {
+        return self.docs.count
+    }
+    
+    func previewController(controller: QLPreviewController!, previewItemAtIndex index: Int) -> QLPreviewItem! {
+        return self.docs[index]
+    }
+
+
+    
+}
