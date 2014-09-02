d7bf51e672c199c2507011c51de5f2cd19cb9f9b
Matt Neuburg <matt@tidbits.com>
2014-09-01 19:36:01 -0700

    revert to handling M_PI individually

--- bk2ch01p017transform/ch14p371frame/AppDelegate.swift
+++ bk2ch01p017transform/ch14p371frame/AppDelegate.swift
@@ -12,7 +12,6 @@ import UIKit
         
         self.window!.rootViewController = UIViewController()
         let mainview = self.window!.rootViewController.view
-        let MPI = CGFloat(M_PI) // shut the compiler up
         
         let which = 1
         switch which {
@@ -24,7 +23,7 @@ import UIKit
             mainview.addSubview(v1)
             v1.addSubview(v2)
             
-            v1.transform = CGAffineTransformMakeRotation(45 * MPI/180.0)
+            v1.transform = CGAffineTransformMakeRotation(45 * CGFloat(M_PI)/180.0)
             println(v1.frame)
             
         case 2:
@@ -46,7 +45,7 @@ import UIKit
             v1.addSubview(v2)
             
             v2.transform = CGAffineTransformMakeTranslation(100, 0)
-            v2.transform = CGAffineTransformRotate(v2.transform, 45 * MPI/180.0)
+            v2.transform = CGAffineTransformRotate(v2.transform, 45 * CGFloat(M_PI)/180.0)
             
         case 4:
             let v1 = UIView(frame:CGRectMake(20, 111, 132, 194))
@@ -56,7 +55,7 @@ import UIKit
             mainview.addSubview(v1)
             v1.addSubview(v2)
             
-            v2.transform = CGAffineTransformMakeRotation(45 * MPI/180.0)
+            v2.transform = CGAffineTransformMakeRotation(45 * CGFloat(M_PI)/180.0)
             v2.transform = CGAffineTransformTranslate(v2.transform, 100, 0)
             
         case 5: // same as case 4 but using concat
@@ -67,7 +66,7 @@ import UIKit
             mainview.addSubview(v1)
             v1.addSubview(v2)
             
-            let r = CGAffineTransformMakeRotation(45 * MPI/180.0)
+            let r = CGAffineTransformMakeRotation(45 * CGFloat(M_PI)/180.0)
             let t = CGAffineTransformMakeTranslation(100, 0)
             v2.transform = CGAffineTransformConcat(t,r) // not r,t
             
@@ -79,7 +78,7 @@ import UIKit
             mainview.addSubview(v1)
             v1.addSubview(v2)
 
-            let r = CGAffineTransformMakeRotation(45 * MPI/180.0)
+            let r = CGAffineTransformMakeRotation(45 * CGFloat(M_PI)/180.0)
             let t = CGAffineTransformMakeTranslation(100, 0)
             v2.transform = CGAffineTransformConcat(t,r)
             v2.transform = CGAffineTransformConcat(CGAffineTransformInvert(r), v2.transform)
