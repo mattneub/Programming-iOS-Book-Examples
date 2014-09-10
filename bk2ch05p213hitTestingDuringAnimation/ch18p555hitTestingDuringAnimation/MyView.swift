8c62be3a6ccf484de1a466954d42e56930d25741
Matt Neuburg <matt@tidbits.com>
2014-09-10 16:28:17 -0700

    whole new way of hit-testing an animated view (thanks, Apple!)

--- bk2ch05p213hitTestingDuringAnimation/ch18p555hitTestingDuringAnimation/MyView.swift
+++ bk2ch05p213hitTestingDuringAnimation/ch18p555hitTestingDuringAnimation/MyView.swift
@@ -18,23 +18,5 @@ class MyView : UIView {
     func tap(g:UIGestureRecognizer!) {
         println("tap! (gesture recognizer)")
     }
-    
-    override func hitTest(point: CGPoint, withEvent event: UIEvent!) -> UIView? {
-        // uncomment this next line to see what it's like without our munged hit-testing
-        // return super.hitTest(point, withEvent: event)
-        // v is the animated subview
-        if let lay = self.v.layer.presentationLayer() as? CALayer {
-            if let hitLayer = lay.hitTest(point) {
-                if (hitLayer == lay || hitLayer.superlayer == lay) {
-                    return self.v
-                }
-            }
-        }
-        let hitView = super.hitTest(point, withEvent:event)
-        if (hitView == self.v) {
-            return self
-        }
-        return hitView
-    }
-    
+        
 }
