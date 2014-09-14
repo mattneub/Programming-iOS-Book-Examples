d715e7084c01da8f9b31438920f0cbdd701ab27f
Matt Neuburg <matt@tidbits.com>
2014-09-13 19:42:42 -0700

    minor; for even greater clarity, remove commented-out lines

--- bk2ch04p151customAnimatableProperty/ch17p498customAnimatableProperty/ViewController.swift
+++ bk2ch04p151customAnimatableProperty/ch17p498customAnimatableProperty/ViewController.swift
@@ -9,11 +9,9 @@ class ViewController : UIViewController {
         let lay = self.v.layer as MyLayer
         let cur = lay.thickness
         let val : CGFloat = cur == 10 ? 0 : 10
-        // CATransaction.setDisableActions(true)
         lay.thickness = val
         let ba = CABasicAnimation(keyPath:"thickness")
         ba.fromValue = cur
-        // ba.toValue = val
         lay.addAnimation(ba, forKey:nil)
     }
     
