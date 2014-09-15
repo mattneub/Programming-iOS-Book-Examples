e3eb85a43c979df76771e4e0d9925a9c73ecf072
Matt Neuburg <matt@tidbits.com>
2014-09-15 12:25:38 -0700

    fix formatting, update to GM

--- bk2ch06p242nibInstantiatedViewController/ch19p583nibInstantiatedViewController/AppDelegate.swift
+++ bk2ch06p242nibInstantiatedViewController/ch19p583nibInstantiatedViewController/AppDelegate.swift
@@ -10,8 +10,10 @@ class AppDelegate : UIResponder, UIApplicationDelegate {
         
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
-        let arr = UINib(nibName: "Main", bundle: nil).instantiateWithOwner(nil, options: nil)
-        self.window!.rootViewController = arr[0] as UIViewController
+        let arr = UINib(
+            nibName: "Main", bundle: nil).instantiateWithOwner(
+                nil, options: nil) as [UIViewController]
+        self.window!.rootViewController = arr[0]
         
         self.window!.backgroundColor = UIColor.whiteColor()
         self.window!.makeKeyAndVisible()
