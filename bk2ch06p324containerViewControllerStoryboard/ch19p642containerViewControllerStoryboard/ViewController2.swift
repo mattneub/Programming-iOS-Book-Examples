408ff654fa0c25b9f48801b238e9cfb360f66988
Matt Neuburg <matt@tidbits.com>
2014-09-21 15:28:42 -0700

    update for 6.0.1, remove unused code

--- bk2ch06p324containerViewControllerStoryboard/ch19p642containerViewControllerStoryboard/ViewController2.swift
+++ bk2ch06p324containerViewControllerStoryboard/ch19p642containerViewControllerStoryboard/ViewController2.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class ViewController2 : UIViewController {
     @IBAction func doButton(sender:AnyObject?) {
-        self.presentingViewController.dismissViewControllerAnimated(true, completion: nil)
+        self.presentingViewController!.dismissViewControllerAnimated(true, completion: nil)
     }
 }
 
@@ -18,41 +18,30 @@ extension ViewController2 {
 }
 
 extension ViewController2 : UIViewControllerTransitioningDelegate {
-    func animationControllerForPresentedController(presented: UIViewController!, presentingController presenting: UIViewController!, sourceController source: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         return self
     }
 }
 
 extension ViewController2 : UIViewControllerAnimatedTransitioning {
-    func transitionDuration(transitionContext: UIViewControllerContextTransitioning!) -> NSTimeInterval {
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
         return 0.8
     }
     
-    func animateTransition(transitionContext: UIViewControllerContextTransitioning!) {
-        let vc1 = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)
-        let vc2 = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        let vc2 = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
         
         let con = transitionContext.containerView()
         
-        let r1start = transitionContext.initialFrameForViewController(vc1)
         let r2end = transitionContext.finalFrameForViewController(vc2)
         
-        let v1 = transitionContext.viewForKey(UITransitionContextFromViewKey)
-        let v2 = transitionContext.viewForKey(UITransitionContextToViewKey)
-        
-        // iOS 7 version
-//        let v1 = vc1.view
-//        let v2 = vc2.view
-        
-        // workaround for iOS 8 bug:
-        con.clipsToBounds = true // !!! This line was not necessary in iOS 7
-            
-        
+        let v2 = transitionContext.viewForKey(UITransitionContextToViewKey)!
+                
         var r2start = r2end
         r2start.origin.y -= r2start.size.height
         v2.frame = r2start
         con.addSubview(v2)
-        UIView.animateWithDuration(0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 20, options: nil, animations: {
+        UIView.animateWithDuration(0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 10, options: nil, animations: {
             v2.frame = r2end
             }, completion: {
                 _ in
