cd1db3eb30b2f9688fb7877a750ace935aabfcc9
Matt Neuburg <matt@tidbits.com>
2014-10-07 12:15:32 -0700

    new example showing presented-in-popover bug and workaround

---
+++ bk2ch09p485PopoverPresentingViewController/DeleteMe/ViewController3.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    @IBAction func doDone(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    
+}
