866c23e9ebe8fa5aa294dc875fa16ed937fe0c9e
Matt Neuburg <matt@tidbits.com>
2014-10-12 09:21:29 -0700

    new example illustrating use of custom attribute as private markup

---
+++ bk2ch10p508AttributedTextAsSecretMarking/AttributedTextAsSecretMarking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AttributedTextAsSecretMarking
+//
+//  Created by Matt Neuburg on 2/7/14.
+//  Copyright (c) 2014 Matt Neuburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
