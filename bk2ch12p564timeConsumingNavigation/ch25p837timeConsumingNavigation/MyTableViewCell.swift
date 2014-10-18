6e8288e512f7b2027d06a2a34afd11c1f1fd2c23
Matt Neuburg <matt@tidbits.com>
2014-10-17 17:31:58 -0700

    remove repeated line

--- bk2ch12p564timeConsumingNavigation/ch25p837timeConsumingNavigation/MyTableViewCell.swift
+++ bk2ch12p564timeConsumingNavigation/ch25p837timeConsumingNavigation/MyTableViewCell.swift
@@ -13,8 +13,7 @@ class MyTableViewCell: UITableViewCell {
             v.color = UIColor.yellowColor()
             dispatch_async(dispatch_get_main_queue()) {
                 v.backgroundColor = UIColor(white:0.2, alpha:0.6)
-                }
-            v.backgroundColor = UIColor(white:0.2, alpha:0.6)
+            }
             v.layer.cornerRadius = 10
             v.frame = v.frame.rectByInsetting(dx: -10, dy: -10)
             let cf = self.contentView.convertRect(self.bounds, fromView:self)
