aeb94b78826b366aeb2d03b920d6ba8a5193ce19
Matt Neuburg <matt@tidbits.com>
2014-11-02 12:24:23 -0800

    update for 6.1 final, use String instead of NSString where possible, move unwrapping up one and other code cleanup

--- bk2ch23p798basicFileOperations/ch36p1053basicFileOperations/ViewController.swift
+++ bk2ch23p798basicFileOperations/ch36p1053basicFileOperations/ViewController.swift
@@ -20,9 +20,8 @@ class ViewController: UIViewController {
     @IBAction func doButton2 (sender:AnyObject!) {
         let fm = NSFileManager()
         var err : NSError?
-        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)
-        if docsurl != nil {
-            println(docsurl!)
+        if let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err) {
+            println(docsurl)
         } else {
             println(err)
         }
@@ -31,9 +30,8 @@ class ViewController: UIViewController {
     @IBAction func doButton3 (sender:AnyObject!) {
         let fm = NSFileManager()
         var err : NSError?
-        let suppurl = fm.URLForDirectory(.ApplicationSupportDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)
-        if suppurl != nil {
-            println(suppurl!)
+        if let suppurl = fm.URLForDirectory(.ApplicationSupportDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err) {
+            println(suppurl)
         } else {
             println(err)
         }
@@ -42,9 +40,9 @@ class ViewController: UIViewController {
     @IBAction func doButton4 (sender:AnyObject!) {
         let fm = NSFileManager()
         var err : NSError?
-        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)
+        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)!
         // error-checking omitted
-        let myfolder = docsurl!.URLByAppendingPathComponent("MyFolder")
+        let myfolder = docsurl.URLByAppendingPathComponent("MyFolder")
         var ok = fm.createDirectoryAtURL(myfolder, withIntermediateDirectories: true, attributes: nil, error: &err)
         if !ok {
             println(err)
@@ -68,13 +66,13 @@ class ViewController: UIViewController {
     @IBAction func doButton5 (sender:AnyObject!) {
         let fm = NSFileManager()
         var err : NSError?
-        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)
-        let arr = fm.contentsOfDirectoryAtURL(docsurl!, includingPropertiesForKeys: nil, options: nil, error: &err)
+        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)!
+        let arr = fm.contentsOfDirectoryAtURL(docsurl, includingPropertiesForKeys: nil, options: nil, error: &err)
         if arr == nil {
             println(err)
             return
         }
-        println((arr! as [NSURL]).map{$0.lastPathComponent})
+        (arr! as [NSURL]).map{$0.lastPathComponent}.map(println)
         // ======
 //        self.query.enumerateResultsUsingBlock {
 //            obj, ix, stop in
@@ -85,8 +83,8 @@ class ViewController: UIViewController {
     @IBAction func doButton7 (sender:AnyObject!) {
         let fm = NSFileManager()
         var err : NSError?
-        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)
-        let dir = fm.enumeratorAtURL(docsurl!, includingPropertiesForKeys: nil, options: nil, errorHandler: nil)
+        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)!
+        let dir = fm.enumeratorAtURL(docsurl, includingPropertiesForKeys: nil, options: nil, errorHandler: nil)!
         // this is what you do when an Objective-C enumerable doesn't conform to Swift's SequenceType
         while let f = dir.nextObject() as? NSURL {
             if f.pathExtension == "txt" {
@@ -98,14 +96,15 @@ class ViewController: UIViewController {
     @IBAction func doButton8 (sender:AnyObject!) {
         let fm = NSFileManager()
         var err : NSError?
-        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)
+        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)!
         let moi = Person(firstName: "Matt", lastName: "Neuburg")
         let moidata = NSKeyedArchiver.archivedDataWithRootObject(moi)
-        let moifile = docsurl!.URLByAppendingPathComponent("moi.txt")
-        let which = 2
+        let moifile = docsurl.URLByAppendingPathComponent("moi.txt")
+        let which = 1
         switch which {
         case 1:
             moidata.writeToURL(moifile, atomically: true)
+            // moifile.setResourceValue(true, forKey: NSURLIsExcludedFromBackupKey, error: &err)
         case 2:
             // ==== the NSFileCoordinator way
             let fc = NSFileCoordinator()
@@ -122,21 +121,21 @@ class ViewController: UIViewController {
     @IBAction func doButton9 (sender:AnyObject!) {
         let fm = NSFileManager()
         var err : NSError?
-        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)
-        let moifile = docsurl!.URLByAppendingPathComponent("moi.txt")
-        let which = 2
+        let docsurl = fm.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: &err)!
+        let moifile = docsurl.URLByAppendingPathComponent("moi.txt")
+        let which = 1
         switch which {
         case 1:
-            let persondata = NSData(contentsOfURL: moifile)
+            let persondata = NSData(contentsOfURL: moifile)!
             let person = NSKeyedUnarchiver.unarchiveObjectWithData(persondata) as Person
-            println("\(person.firstName) \(person.lastName)")
+            println(person)
         case 2:
             // ==== the NSFileCoordinator way
             let fc = NSFileCoordinator()
             let intent = NSFileAccessIntent.readingIntentWithURL(moifile, options: nil)
             fc.coordinateAccessWithIntents([intent], queue: NSOperationQueue.mainQueue()) {
                 (err:NSError!) in
-                let persondata = NSData(contentsOfURL: intent.URL)
+                let persondata = NSData(contentsOfURL: intent.URL)!
                 let person = NSKeyedUnarchiver.unarchiveObjectWithData(persondata) as Person
                 println(person)
             }
