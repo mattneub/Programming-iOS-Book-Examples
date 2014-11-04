0870e9c80ec2efa664442450bf55f00c7ec0f83e
Matt Neuburg <matt@tidbits.com>
2014-11-04 09:05:37 -0800

    revise MyDownloader to use wrapper class instead of unsafe cast

--- bk2ch24p842downloader/ch37p1099downloader/MyDownloader.swift
+++ bk2ch24p842downloader/ch37p1099downloader/MyDownloader.swift
@@ -2,15 +2,12 @@
 
 import UIKit
 
-/*
-Passing a Swift block as an Objective-C object turns out to be very tricky.
-You have to designate the block with the undocumented @objc_block attribute,
-so it has the same size as an Objective-C block;
-then you have to cast to AnyObject with the obscure built-in function unsafeBitCast()
-and reverse that when you fetch it out, in order to call it
-*/
+class Wrapper<T> {
+    let p:T
+    init(_ p:T){self.p = p}
+}
 
-typealias MyDownloaderCompletionHandler = @objc_block (NSURL!) -> ()
+typealias MyDownloaderCompletion = (NSURL!) -> ()
 
 class MyDownloader: NSObject, NSURLSessionDownloadDelegate {
     let config : NSURLSessionConfiguration
@@ -26,16 +23,15 @@ class MyDownloader: NSObject, NSURLSessionDownloadDelegate {
         super.init()
     }
     
-    func download(s:String, completionHandler ch : MyDownloaderCompletionHandler) -> NSURLSessionTask {
+    func download(s:String, completionHandler ch : MyDownloaderCompletion) -> NSURLSessionTask {
         let url = NSURL(string:s)!
         let req = NSMutableURLRequest(URL:url)
-        let ch2 : AnyObject = unsafeBitCast(ch, AnyObject.self)
-        NSURLProtocol.setProperty(ch2, forKey:"ch", inRequest:req)
+        NSURLProtocol.setProperty(Wrapper(ch), forKey:"ch", inRequest:req)
         let task = self.session.downloadTaskWithRequest(req)
         task.resume()
         return task
     }
-    
+
     func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten writ: Int64, totalBytesExpectedToWrite exp: Int64) {
         println("downloaded \(100*writ/exp)%")
     }
@@ -56,11 +52,12 @@ class MyDownloader: NSObject, NSURLSessionDownloadDelegate {
             url = location
             println("download \(req.URL.lastPathComponent)")
         }
+        let ch2 = (ch as Wrapper).p as MyDownloaderCompletion
         if self.main {
-            unsafeBitCast(ch, MyDownloaderCompletionHandler.self)(url)
+            ch2(url)
         } else {
             dispatch_sync(dispatch_get_main_queue()) {
-                unsafeBitCast(ch, MyDownloaderCompletionHandler.self)(url)
+                ch2(url)
             }
         }
     }
