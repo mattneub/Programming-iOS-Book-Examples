fd3520fac11e3f4d5cc1f1c67aa8426eb04c63e3
Matt Neuburg <matt@tidbits.com>
2015-04-26 21:18:30 -0700

    add collapse/expand example

---
+++ bk2ch04p183animationAndAutolayout4/Expandable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Expandable
+//
+//  Created by Kevin Sylvestre on 2015-04-17.
+//  Copyright (c) 2015 Kevin Sylvestre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
