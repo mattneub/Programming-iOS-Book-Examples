ff0e5f5420df241c932ec7f3cb46c5d8e0fcf6e4
Matt Neuburg <matt@tidbits.com>
2015-07-12 17:18:33 -0700

    revised chapter 8 example

--- bk1ch08p350documentation/bk1ch08p350documentation/AppDelegate.swift
+++ bk1ch08p350documentation/bk1ch08p350documentation/AppDelegate.swift
@@ -12,7 +12,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-
-
 }
 
--- bk1ch08p350documentation/bk1ch08p350documentation/ViewController.swift
+++ bk1ch08p350documentation/bk1ch08p350documentation/ViewController.swift
@@ -4,19 +4,33 @@ import UIKit
 class ViewController: UIViewController {
 
     /**
-    Many people would like to dog their cats. So it is perfectly
+    Many people would like to dog their cats. So it is *perfectly*
     reasonable to supply a convenience method to do so:
     
     * Because it's cool.
     * Because it's there.
     
-    :param: cats A string containing cats
+    #### Parameters
     
-    :returns: A string containing dogs
+    `cats` A string containing cats
+    
+    #### Return Value
+    
+    A string containing dogs
     */
     
     func dogMyCats(cats:String) -> String {
         return "Dogs"
+    }
+    
+    /// Just showing that this gets Quick Help
+    /// too.
+    
+    func anotherMethod() {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 
 
