1c5063550a38812372988e6d0b78879a440cf00f
Matt Neuburg <matt@tidbits.com>
2015-10-18 13:04:06 -0700

    die CGBitmapInfo die die die

--- bk2ch05p212hitTestingDistortedLayer/ch18p554hitTestingDistortedLayer/MyView.swift
+++ bk2ch05p212hitTestingDistortedLayer/ch18p554hitTestingDistortedLayer/MyView.swift
@@ -49,11 +49,11 @@ class MyView : UIView {
         let im = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        let info = CGBitmapInfo(rawValue: CGImageAlphaInfo.Only.rawValue)
+        let info = CGImageAlphaInfo.Only.rawValue
         let pixel = UnsafeMutablePointer<CUnsignedChar>.alloc(1)
         pixel[0] = 0
         let context = CGBitmapContextCreate(pixel,
-            1, 1, 8, 1, nil, info.rawValue)!
+            1, 1, 8, 1, nil, info)!
         UIGraphicsPushContext(context)
         im.drawAtPoint(CGPointMake(-point.x, -point.y))
         UIGraphicsPopContext()
