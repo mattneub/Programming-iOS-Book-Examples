0f7f1ae50eb2bca805cc681a313c2fd41e920305
Matt Neuburg <matt@tidbits.com>
2016-09-28 07:28:33 -0700

    new variable row heights example

---
+++ bk2ch08p425variableHeights2/VariableRowHeights/AppDelegate.swift
@@ -0,0 +1,18 @@
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
