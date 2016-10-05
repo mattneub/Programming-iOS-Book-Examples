85f8476450de78bac0ff646ccaa537e529951893
Matt Neuburg <matt@tidbits.com>
2016-10-04 20:13:26 -0700

    chapter 19 examples now compile

--- bk2ch19p725calendar/ch32p986calendar/ViewController.swift
+++ bk2ch19p725calendar/ch32p986calendar/ViewController.swift
@@ -9,77 +9,67 @@ func delay(_ delay:Double, closure:@escaping ()->()) {
     DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
 }
 
-func lend<T where T:NSObject> (closure:(T)->()) -> T {
+func lend<T> (closure:(T)->()) -> T where T:NSObject {
     let orig = T()
     closure(orig)
     return orig
 }
 
+let database = EKEventStore()
+
+func checkForEventAccess(andThen f:(()->())? = nil) {
+    let status = EKEventStore.authorizationStatus(for:.event)
+    switch status {
+    case .authorized:
+        f?()
+    case .notDetermined:
+        database.requestAccess(to:.event) { ok, err in
+            if ok {
+                DispatchQueue.main.async {
+                    f?()
+                }
+            }
+        }
+    case .restricted:
+        // do nothing
+        break
+    case .denied:
+        // do nothing, or beg the user to authorize us in Settings
+        print("denied")
+        break
+    }
+}
+
+
 class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDelegate, EKCalendarChooserDelegate {
-    let database = EKEventStore()
     var napid : String!
 
-    @discardableResult
-    func determineStatus() -> Bool {
-        let type = EKEntityType.event
-        let stat = EKEventStore.authorizationStatus(for:type)
-        switch stat {
-        case .authorized:
-            return true
-        case .notDetermined:
-            self.database.requestAccess(to:type){_ in}
-            return false
-        case .restricted:
-            return false
-        case .denied:
-            // new iOS 8 feature: sane way of getting the user directly to the relevant prefs
-            // I think the crash-in-background issue is now gone
-            let alert = UIAlertController(title: "Need Authorization", message: "Wouldn't you like to authorize this app to use your Calendar?", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "No", style: .cancel))
-            alert.addAction(UIAlertAction(title: "OK", style: .default) {
-                _ in
-                let url = URL(string:UIApplicationOpenSettingsURLString)!
-                UIApplication.shared.open(url)
-            })
-            self.present(alert, animated:true)
-            return false
-        }
-    }
-
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        self.determineStatus()
-        NotificationCenter.default.addObserver(self, selector: #selector(determineStatus), name: .UIApplicationWillEnterForeground, object: nil)
+    var database : EKEventStore {
+        return MyCalendarApp.database
     }
     
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        NotificationCenter.default.removeObserver(self)
-    }
-
     @IBAction func createCalendar (_ sender: Any!) {
-        if !self.determineStatus() {
-            print("not authorized")
-            return
+        checkForEventAccess {
+        
+            // obtain local source
+            let locals = self.database.sources.filter {$0.sourceType == .local}
+            guard let src = locals.first else {
+                print("failed to find local source")
+                return
+            }
+            let cal = EKCalendar(for:.event, eventStore:self.database)
+            cal.source = src
+            cal.title = "CoolCal"
+            // ready to save the new calendar into the database!
+            do {
+                try self.database.saveCalendar(cal, commit:true)
+            } catch {
+                print("save calendar error: \(error)")
+                return
+            }
+            print("no errors")
+            
         }
-        // obtain local source
-        let locals = self.database.sources.filter {$0.sourceType == .local}
-        guard let src = locals.first else {
-            print("failed to find local source")
-            return
-        }
-        let cal = EKCalendar(for:.event, eventStore:self.database)
-        cal.source = src
-        cal.title = "CoolCal"
-        // ready to save the new calendar into the database!
-        do {
-            try self.database.saveCalendar(cal, commit:true)
-        } catch {
-            print("save calendar error: \(error)")
-            return
-        }
-        print("no errors")
     }
 
     func calendar(name:String ) -> EKCalendar? {
@@ -88,159 +78,159 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
     }
     
     @IBAction func createSimpleEvent (_ sender: Any!) {
-        if !self.determineStatus() {
-            print("not authorized")
-            return
+        
+        checkForEventAccess {
+        
+            guard let cal = self.calendar(name:"CoolCal") else {
+                print("failed to find calendar")
+                return
+            }
+            
+            let greg = Calendar(identifier:.gregorian)
+            var comp = DateComponents(year:1016, month:8, day:10, hour:15)
+            let d1 = greg.date(from:comp)!
+            comp.hour = comp.hour! + 1
+            let d2 = greg.date(from:comp)!
+            
+            let ev = EKEvent(eventStore:self.database)
+            ev.title = "Take a nap"
+            ev.notes = "You deserve it!"
+            ev.calendar = cal
+            (ev.startDate, ev.endDate) = (d1,d2)
+            
+            // we can also easily add an alarm
+            let alarm = EKAlarm(relativeOffset:-3600) // one hour before
+            ev.addAlarm(alarm)
+            
+            do {
+                try self.database.save(ev, span:.thisEvent, commit:true)
+            } catch {
+                print("save simple event \(error)")
+                return
+            }
+            print("no errors")
+                
         }
-        
-        guard let cal = self.calendar(name:"CoolCal") else {
-            print("failed to find calendar")
-            return
-        }
-        
-        let greg = Calendar(calendarIdentifier:.gregorian)!
-        var comp = DateComponents(year:1016, month:8, day:10, hour:15)
-        let d1 = greg.date(from:comp)!
-        comp.hour = comp.hour! + 1
-        let d2 = greg.date(from:comp)!
-        
-        let ev = EKEvent(eventStore:self.database)
-        ev.title = "Take a nap"
-        ev.notes = "You deserve it!"
-        ev.calendar = cal
-        (ev.startDate, ev.endDate) = (d1,d2)
-        
-        // we can also easily add an alarm
-        let alarm = EKAlarm(relativeOffset:-3600) // one hour before
-        ev.addAlarm(alarm)
-        
-        do {
-            try self.database.save(ev, span:.thisEvent, commit:true)
-        } catch {
-            print("save simple event \(error)")
-            return
-        }
-        print("no errors")
     }
 
     @IBAction func createRecurringEvent (_ sender: Any!) {
-        if !self.determineStatus() {
-            print("not authorized")
-            return
-        }
         
-        guard let cal = self.calendar(name:"CoolCal") else {
-            print("failed to find calendar")
-            return
-        }
+        checkForEventAccess {
+        
+            guard let cal = self.calendar(name:"CoolCal") else {
+                print("failed to find calendar")
+                return
+            }
 
-        let everySunday = EKRecurrenceDayOfWeek(.sunday)
-        let january = 1
-        let recur = EKRecurrenceRule(
-            recurrenceWith:.yearly, // every year
-            interval:2, // no, every *two* years
-            daysOfTheWeek:[everySunday],
-            daysOfTheMonth:nil,
-            monthsOfTheYear:[january],
-            weeksOfTheYear:nil,
-            daysOfTheYear:nil,
-            setPositions: nil,
-            end:nil)
-        
-        let ev = EKEvent(eventStore:self.database)
-        ev.title = "Mysterious biennial Sunday-in-January morning ritual"
-        ev.addRecurrenceRule(recur)
-        ev.calendar = cal
-        // need a start date and end date
-        let greg = Calendar(calendarIdentifier:.gregorian)!
-        var comp = DateComponents(year:2016, month:1, hour:10)
-        comp.weekday = 1 // Sunday
-        comp.weekdayOrdinal = 1 // *first* Sunday
-        ev.startDate = greg.date(from:comp)!
-        comp.hour = 11
-        ev.endDate = greg.date(from:comp)!
-        
-        do {
-            try self.database.save(ev, span:.futureEvents, commit:true)
-        } catch {
-            print("save recurring event \(error)")
-            return
+            let everySunday = EKRecurrenceDayOfWeek(.sunday)
+            let january = 1
+            let recur = EKRecurrenceRule(
+                recurrenceWith:.yearly, // every year
+                interval:2, // no, every *two* years
+                daysOfTheWeek:[everySunday],
+                daysOfTheMonth:nil,
+                monthsOfTheYear:[january as NSNumber], // ?
+                weeksOfTheYear:nil,
+                daysOfTheYear:nil,
+                setPositions: nil,
+                end:nil)
+            
+            let ev = EKEvent(eventStore:self.database)
+            ev.title = "Mysterious biennial Sunday-in-January morning ritual"
+            ev.addRecurrenceRule(recur)
+            ev.calendar = cal
+            // need a start date and end date
+            let greg = Calendar(identifier:.gregorian)
+            var comp = DateComponents(year:2016, month:1, hour:10)
+            comp.weekday = 1 // Sunday
+            comp.weekdayOrdinal = 1 // *first* Sunday
+            ev.startDate = greg.date(from:comp)!
+            comp.hour = 11
+            ev.endDate = greg.date(from:comp)!
+            
+            do {
+                try self.database.save(ev, span:.futureEvents, commit:true)
+            } catch {
+                print("save recurring event \(error)")
+                return
+            }
+            print("no errors")
+            
         }
-        print("no errors")
 
     }
     
     @IBAction func searchByRange (_ sender: Any!) {
-        if !self.determineStatus() {
-            print("not authorized")
-            return
-        }
         
-        guard let cal = self.calendar(name:"CoolCal") else {
-            print("failed to find calendar")
-            return
-        }
+        checkForEventAccess {
         
-        let d1 = Date() // today
-        let greg = Calendar(calendarIdentifier:.gregorian)!
-        let d2 = greg.date(byAdding: lend {
-            (comp:NSDateComponents) in comp.year = 2
-            } as DateComponents, to:d1)!
-        let pred = self.database.predicateForEvents(withStart:
-            d1, end:d2, calendars:[cal])
-        var events = [EKEvent]()
-        DispatchQueue.global(attributes:.qosDefault).async {
-            self.database.enumerateEvents(matching:pred) {
-                (event:EKEvent, stop:UnsafeMutablePointer<ObjCBool>) in
-                events += [event]
-                if event.title.range(of:"nap") != nil {
-                    self.napid = event.calendarItemIdentifier
-                    print("found the nap")
-                    // stop.memory = true
+            guard let cal = self.calendar(name:"CoolCal") else {
+                print("failed to find calendar")
+                return
+            }
+            
+            let d1 = Date() // today
+            let greg = Calendar(identifier:.gregorian)
+            let d2 = greg.date(byAdding: lend {
+                (comp:NSDateComponents) in comp.year = 2
+                } as DateComponents, to:d1)!
+            let pred = self.database.predicateForEvents(withStart:
+                d1, end:d2, calendars:[cal])
+            var events = [EKEvent]()
+            DispatchQueue.global(qos:.default).async {
+                self.database.enumerateEvents(matching:pred) {
+                    (event:EKEvent, stop:UnsafeMutablePointer<ObjCBool>) in
+                    events += [event]
+                    if event.title.range(of:"nap") != nil {
+                        self.napid = event.calendarItemIdentifier
+                        print("found the nap")
+                        // stop.pointee = true
+                    }
                 }
+                events.sort {
+                    $0.compareStartDate(with:$1) == .orderedAscending
+                }
+                print(events)
+                print(events.map {$0.calendarItemIdentifier})
             }
-            events.sort {
-                $0.compareStartDate(with:$1) == .orderedAscending
-            }
-            print(events)
-            print(events.map {$0.calendarItemIdentifier})
+            
         }
     }
     
     // ========
 
     @IBAction func showEventUI (_ sender: Any!) {
-        if !self.determineStatus() {
-            print("not authorized")
-            return
-        }
-        if self.napid == nil {
-            print("need to search for nap event first")
-            return
-        }
-        let ev = self.database.calendarItem(withIdentifier:self.napid) as! EKEvent
+        checkForEventAccess {
+        
+            if self.napid == nil {
+                print("need to search for nap event first")
+                return
+            }
+            let ev = self.database.calendarItem(withIdentifier:self.napid) as! EKEvent
 
-        let evc = EKEventViewController()
-        evc.event = ev
-        evc.allowsEditing = true
-        evc.delegate = self
-        // big big change
-        self.navigationController?.pushViewController(evc, animated: true)
-//        let nav = UINavigationController(rootViewController: evc)
-//        nav.modalPresentationStyle = .popover
-//        self.present(nav, animated: true)
-//        if let pop = nav.popoverPresentationController {
-//            if let v = sender as? UIView {
-//                pop.sourceView = v
-//                pop.sourceRect = v.bounds
-//            }
-//        }
+            let evc = EKEventViewController()
+            evc.event = ev
+            evc.allowsEditing = true
+            evc.delegate = self
+            // big big change
+            self.navigationController?.pushViewController(evc, animated: true)
+    //        let nav = UINavigationController(rootViewController: evc)
+    //        nav.modalPresentationStyle = .popover
+    //        self.present(nav, animated: true)
+    //        if let pop = nav.popoverPresentationController {
+    //            if let v = sender as? UIView {
+    //                pop.sourceView = v
+    //                pop.sourceRect = v.bounds
+    //            }
+    //        }
+            
+        }
     }
 
     func eventViewController(_ controller: EKEventViewController,
         didCompleteWith action: EKEventViewAction) {
             print("did complete with action \(action.rawValue)")
-            if action == .deleted { // _ = in next line is due to bug, will be removable soon
+            if action == .deleted { // _ = in next line is due to optional, I regard as bug
                 _ = self.navigationController?.popViewController(animated:true)
             }
     }
@@ -251,16 +241,20 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
     // this interface will appear with a lock icon and the user must cancel
 
     @IBAction func editEvent (_ sender: Any!) {
-        let evc = EKEventEditViewController()
-        evc.eventStore = self.database
-        evc.editViewDelegate = self
-        evc.modalPresentationStyle = .popover
-        self.present(evc, animated: true)
-        if let pop = evc.popoverPresentationController {
-            if let v = sender as? UIView {
-                pop.sourceView = v
-                pop.sourceRect = v.bounds
+        checkForEventAccess {
+        
+            let evc = EKEventEditViewController()
+            evc.eventStore = self.database
+            evc.editViewDelegate = self
+            evc.modalPresentationStyle = .popover
+            self.present(evc, animated: true)
+            if let pop = evc.popoverPresentationController {
+                if let v = sender as? UIView {
+                    pop.sourceView = v
+                    pop.sourceRect = v.bounds
+                }
             }
+            
         }
     }
     
@@ -279,22 +273,26 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
     // this one too
 
     @IBAction func deleteCalendar (_ sender: Any!) {
-        let choo = EKCalendarChooser(
-            selectionStyle:.single,
-            displayStyle:.allCalendars,
-            entityType:.event,
-            eventStore:self.database)
-        choo.showsDoneButton = true
-        choo.showsCancelButton = true
-        choo.delegate = self
-        let nav = UINavigationController(rootViewController: choo)
-        nav.modalPresentationStyle = .popover
-        self.present(nav, animated: true)
-        if let pop = nav.popoverPresentationController {
-            if let v = sender as? UIView {
-                pop.sourceView = v
-                pop.sourceRect = v.bounds
+        checkForEventAccess {
+        
+            let choo = EKCalendarChooser(
+                selectionStyle:.single,
+                displayStyle:.allCalendars,
+                entityType:.event,
+                eventStore:self.database)
+            choo.showsDoneButton = true
+            choo.showsCancelButton = true
+            choo.delegate = self
+            let nav = UINavigationController(rootViewController: choo)
+            nav.modalPresentationStyle = .popover
+            self.present(nav, animated: true)
+            if let pop = nav.popoverPresentationController {
+                if let v = sender as? UIView {
+                    pop.sourceView = v
+                    pop.sourceRect = v.bounds
+                }
             }
+            
         }
     }
     
--- bk2ch19p731reminders/ch32p994reminders/ViewController.swift
+++ bk2ch19p731reminders/ch32p994reminders/ViewController.swift
@@ -8,89 +8,82 @@ func delay(_ delay:Double, closure:@escaping ()->()) {
     DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
 }
 
+let database = EKEventStore()
+
+func checkForReminderAccess(andThen f:(()->())? = nil) {
+    let status = EKEventStore.authorizationStatus(for:.reminder)
+    switch status {
+    case .authorized:
+        f?()
+    case .notDetermined:
+        database.requestAccess(to:.reminder) { ok, err in
+            if ok {
+                DispatchQueue.main.async {
+                    f?()
+                }
+            }
+        }
+    case .restricted:
+        // do nothing
+        break
+    case .denied:
+        // do nothing, or beg the user to authorize us in Settings
+        print("denied")
+        break
+    }
+}
+
 
 class ViewController: UIViewController {
     
-    let database = EKEventStore()
-
-    @discardableResult
-    func determineStatus() -> Bool {
-        let type = EKEntityType.reminder // *
-        let stat = EKEventStore.authorizationStatus(for:type)
-        switch stat {
-        case .authorized:
-            return true
-        case .notDetermined:
-            database.requestAccess(to:type) {_ in}
-            return false
-        case .restricted:
-            return false
-        case .denied:
-            // new iOS 8 feature: sane way of getting the user directly to the relevant prefs
-            // I think the crash-in-background issue is now gone
-            let alert = UIAlertController(title: "Need Authorization", message: "Wouldn't you like to authorize this app to use your Calendar?", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "No", style: .cancel))
-            alert.addAction(UIAlertAction(title: "OK", style: .default) {
-                _ in
-                let url = URL(string:UIApplicationOpenSettingsURLString)!
-                UIApplication.shared.open(url)
-            })
-            self.present(alert, animated:true)
-            return false
-        }
+    var database : EKEventStore {
+        return MyReminderApp.database
     }
     
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        self.determineStatus()
-        NotificationCenter.default.addObserver(self, selector: #selector(determineStatus), name: .UIApplicationWillEnterForeground, object: nil)
-    }
-
     @IBAction func doNewReminder (_ sender: Any!) {
-        if !self.determineStatus() {
-            print("not authorized")
-            return
+        
+        checkForReminderAccess {
+        
+            let cal = self.database.defaultCalendarForNewReminders()
+            
+            let rem = EKReminder(eventStore:self.database)
+            rem.title = "Get bread"
+            rem.calendar = cal
+            
+            // reminder can have due date
+            // let's make it today
+            let today = Date()
+            let greg = Calendar(identifier:.gregorian)
+            // day without time means "all day"
+            let comps : Set<Calendar.Component> = [.year, .month, .day]
+            // start date not needed on iOS
+            // rem.startDateComponents = [greg components:comps fromDate:today];
+            rem.dueDateComponents = greg.dateComponents(comps, from:today)
+            
+            // let's add an alarm
+            // interesting use of location: we can make a location-based alarm
+            let alarm = EKAlarm()
+            let loc = EKStructuredLocation(title:"Trader Joe's")
+            loc.geoLocation = CLLocation(latitude:34.271848, longitude:-119.247714)
+            loc.radius = 10*1000 // metres
+            alarm.structuredLocation = loc
+            alarm.proximity = .enter // "geofence": we alarm when *arriving*
+            // but this will have no effect until Reminders is granted Location access...
+            // and in iOS 8 it won't even ask for it until it is launched
+            // also, in iOS 8 the separate background usage pref is withdrawn;
+            // instead, auth of Reminders for "when in use" covers this...
+            // ...because it means "this app *or one of its features* is visible on screen"
+            rem.addAlarm(alarm)
+            
+            do {
+                try self.database.save(rem, commit:true)
+            } catch {
+                print("save calendar \(error)")
+                return
+            }
+            print("no error")
+            
         }
-        
-        let cal = self.database.defaultCalendarForNewReminders()
-        
-        let rem = EKReminder(eventStore:self.database)
-        rem.title = "Get bread"
-        rem.calendar = cal
-        
-        // reminder can have due date
-        // let's make it today
-        let today = Date()
-        let greg = Calendar(calendarIdentifier:.gregorian)!
-        // day without time means "all day"
-        let comps : Calendar.Unit = [.year, .month, .day]
-        // start date not needed on iOS
-        // rem.startDateComponents = [greg components:comps fromDate:today];
-        rem.dueDateComponents = greg.components(comps, from:today)
-        
-        // let's add an alarm
-        // interesting use of location: we can make a location-based alarm
-        let alarm = EKAlarm()
-        let loc = EKStructuredLocation(title:"Trader Joe's")
-        loc.geoLocation = CLLocation(latitude:34.271848, longitude:-119.247714)
-        loc.radius = 10*1000 // metres
-        alarm.structuredLocation = loc
-        alarm.proximity = .enter // "geofence": we alarm when *arriving*
-        // but this will have no effect until Reminders is granted Location access...
-        // and in iOS 8 it won't even ask for it until it is launched
-        // also, in iOS 8 the separate background usage pref is withdrawn;
-        // instead, auth of Reminders for "when in use" covers this...
-        // ...because it means "this app *or one of its features* is visible on screen"
-        rem.addAlarm(alarm)
-        
-        do {
-            try self.database.save(rem, commit:true)
-        } catch {
-            print("save calendar \(error)")
-            return
-        }
-        print("no error")
     }
 
 
