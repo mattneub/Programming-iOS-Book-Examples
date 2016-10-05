77845b2010efae4858d76b99324ed302d39467db
Matt Neuburg <matt@tidbits.com>
2016-10-05 10:01:05 -0700

    finished revising chapter 19 examples

--- bk2ch19p725calendar/ch32p986calendar/ViewController.swift
+++ bk2ch19p725calendar/ch32p986calendar/ViewController.swift
@@ -50,25 +50,23 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
     
     @IBAction func createCalendar (_ sender: Any!) {
         checkForEventAccess {
-        
-            // obtain local source
-            let locals = self.database.sources.filter {$0.sourceType == .local}
-            guard let src = locals.first else {
-                print("failed to find local source")
-                return
-            }
-            let cal = EKCalendar(for:.event, eventStore:self.database)
-            cal.source = src
-            cal.title = "CoolCal"
-            // ready to save the new calendar into the database!
             do {
+                // obtain local source
+                let locals = self.database.sources.filter {$0.sourceType == .local}
+                guard let src = locals.first else {
+                    print("failed to find local source")
+                    return
+                }
+                let cal = EKCalendar(for:.event, eventStore:self.database)
+                cal.source = src
+                cal.title = "CoolCal"
+                // ready to save the new calendar into the database!
                 try self.database.saveCalendar(cal, commit:true)
+                print("no errors")
             } catch {
                 print("save calendar error: \(error)")
                 return
             }
-            print("no errors")
-            
         }
     }
 
@@ -80,81 +78,89 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
     @IBAction func createSimpleEvent (_ sender: Any!) {
         
         checkForEventAccess {
-        
-            guard let cal = self.calendar(name:"CoolCal") else {
-                print("failed to find calendar")
-                return
-            }
-            
-            let greg = Calendar(identifier:.gregorian)
-            var comp = DateComponents(year:1016, month:8, day:10, hour:15)
-            let d1 = greg.date(from:comp)!
-            comp.hour = comp.hour! + 1
-            let d2 = greg.date(from:comp)!
-            
-            let ev = EKEvent(eventStore:self.database)
-            ev.title = "Take a nap"
-            ev.notes = "You deserve it!"
-            ev.calendar = cal
-            (ev.startDate, ev.endDate) = (d1,d2)
-            
-            // we can also easily add an alarm
-            let alarm = EKAlarm(relativeOffset:-3600) // one hour before
-            ev.addAlarm(alarm)
             
             do {
+                
+                guard let cal = self.calendar(name:"CoolCal") else {
+                    print("failed to find calendar")
+                    return
+                }
+                // form the start and end dates
+                let greg = Calendar(identifier:.gregorian)
+                var comp = DateComponents(year:2016, month:8, day:10, hour:15)
+                let d1 = greg.date(from:comp)!
+                comp.hour = comp.hour! + 1
+                let d2 = greg.date(from:comp)!
+                
+                // form the event
+                let ev = EKEvent(eventStore:self.database)
+                ev.title = "Take a nap"
+                ev.notes = "You deserve it!"
+                ev.calendar = cal
+                (ev.startDate, ev.endDate) = (d1,d2)
+                
+                // we can also easily add an alarm
+                let alarm = EKAlarm(relativeOffset:-3600) // one hour before
+                ev.addAlarm(alarm)
+                
+                // save it
                 try self.database.save(ev, span:.thisEvent, commit:true)
+                print("no errors")
+                
             } catch {
                 print("save simple event \(error)")
                 return
             }
-            print("no errors")
-                
+
+            
         }
     }
 
     @IBAction func createRecurringEvent (_ sender: Any!) {
         
         checkForEventAccess {
-        
-            guard let cal = self.calendar(name:"CoolCal") else {
-                print("failed to find calendar")
-                return
-            }
-
-            let everySunday = EKRecurrenceDayOfWeek(.sunday)
-            let january = 1
-            let recur = EKRecurrenceRule(
-                recurrenceWith:.yearly, // every year
-                interval:2, // no, every *two* years
-                daysOfTheWeek:[everySunday],
-                daysOfTheMonth:nil,
-                monthsOfTheYear:[january as NSNumber], // ?
-                weeksOfTheYear:nil,
-                daysOfTheYear:nil,
-                setPositions: nil,
-                end:nil)
-            
-            let ev = EKEvent(eventStore:self.database)
-            ev.title = "Mysterious biennial Sunday-in-January morning ritual"
-            ev.addRecurrenceRule(recur)
-            ev.calendar = cal
-            // need a start date and end date
-            let greg = Calendar(identifier:.gregorian)
-            var comp = DateComponents(year:2016, month:1, hour:10)
-            comp.weekday = 1 // Sunday
-            comp.weekdayOrdinal = 1 // *first* Sunday
-            ev.startDate = greg.date(from:comp)!
-            comp.hour = 11
-            ev.endDate = greg.date(from:comp)!
             
             do {
+        
+                guard let cal = self.calendar(name:"CoolCal") else {
+                    print("failed to find calendar")
+                    return
+                }
+
+                let everySunday = EKRecurrenceDayOfWeek(.sunday)
+                let january = 1 as NSNumber
+                let recur = EKRecurrenceRule(
+                    recurrenceWith:.yearly, // every year
+                    interval:2, // no, every *two* years
+                    daysOfTheWeek:[everySunday],
+                    daysOfTheMonth:nil,
+                    monthsOfTheYear:[january],
+                    weeksOfTheYear:nil,
+                    daysOfTheYear:nil,
+                    setPositions: nil,
+                    end:nil)
+                
+                let ev = EKEvent(eventStore:self.database)
+                ev.title = "Mysterious biennial Sunday-in-January morning ritual"
+                ev.addRecurrenceRule(recur)
+                ev.calendar = cal
+                // need a start date and end date
+                let greg = Calendar(identifier:.gregorian)
+                var comp = DateComponents(year:2016, month:1, hour:10)
+                comp.weekday = 1 // Sunday
+                comp.weekdayOrdinal = 1 // *first* Sunday
+                ev.startDate = greg.date(from:comp)!
+                comp.hour = 11
+                ev.endDate = greg.date(from:comp)!
+                
                 try self.database.save(ev, span:.futureEvents, commit:true)
+                print("no errors")
+                
             } catch {
                 print("save recurring event \(error)")
                 return
             }
-            print("no errors")
+
             
         }
 
@@ -169,35 +175,40 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
                 return
             }
             
-            let d1 = Date() // today
             let greg = Calendar(identifier:.gregorian)
+            let d = Date() // today
+            let d1 = greg.date(byAdding: lend {
+                (comp:NSDateComponents) in comp.year = -1
+            } as DateComponents, to:d)!
             let d2 = greg.date(byAdding: lend {
                 (comp:NSDateComponents) in comp.year = 2
-                } as DateComponents, to:d1)!
+            } as DateComponents, to:d1)!
             let pred = self.database.predicateForEvents(withStart:
                 d1, end:d2, calendars:[cal])
-            var events = [EKEvent]()
             DispatchQueue.global(qos:.default).async {
-                self.database.enumerateEvents(matching:pred) {
-                    (event:EKEvent, stop:UnsafeMutablePointer<ObjCBool>) in
-                    events += [event]
-                    if event.title.range(of:"nap") != nil {
-                        self.napid = event.calendarItemIdentifier
+                var events = [EKEvent]()
+                self.database.enumerateEvents(matching:pred) { ev, stop in
+                    events += [ev]
+                    if ev.title.range(of:"nap") != nil {
+                        self.napid = ev.calendarItemIdentifier
                         print("found the nap")
-                        // stop.pointee = true
+                        stop.pointee = true
                     }
                 }
-                events.sort {
-                    $0.compareStartDate(with:$1) == .orderedAscending
-                }
+                events.sort { $0.compareStartDate(with:$1) == .orderedAscending }
+                
                 print(events)
                 print(events.map {$0.calendarItemIdentifier})
+                
+
             }
             
         }
     }
     
     // ========
+    
+    var napEvent : EKEvent!
 
     @IBAction func showEventUI (_ sender: Any!) {
         checkForEventAccess {
@@ -210,7 +221,7 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
 
             let evc = EKEventViewController()
             evc.event = ev
-            evc.allowsEditing = true
+            evc.allowsEditing = false
             evc.delegate = self
             // big big change
             self.navigationController?.pushViewController(evc, animated: true)
@@ -231,15 +242,13 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
         didCompleteWith action: EKEventViewAction) {
             print("did complete with action \(action.rawValue)")
             if action == .deleted { // _ = in next line is due to optional, I regard as bug
-                _ = self.navigationController?.popViewController(animated:true)
+                // changing to forced unwrap to avoid that
+                self.navigationController!.popViewController(animated:true)
             }
     }
 
     // ========
     
-    // like the photo interface, if there is no access
-    // this interface will appear with a lock icon and the user must cancel
-
     @IBAction func editEvent (_ sender: Any!) {
         checkForEventAccess {
         
@@ -270,8 +279,6 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
 
     // ===============
 
-    // this one too
-
     @IBAction func deleteCalendar (_ sender: Any!) {
         checkForEventAccess {
         
@@ -283,6 +290,7 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
             choo.showsDoneButton = true
             choo.showsCancelButton = true
             choo.delegate = self
+            choo.navigationItem.prompt = "Pick a calendar to delete:"
             let nav = UINavigationController(rootViewController: choo)
             nav.modalPresentationStyle = .popover
             self.present(nav, animated: true)
@@ -298,31 +306,26 @@ class ViewController: UIViewController, EKEventViewDelegate, EKEventEditViewDele
     
     // need delegate methods in order to dismiss
 
-    func calendarChooserDidCancel(_ calendarChooser: EKCalendarChooser) {
+    func calendarChooserDidCancel(_ choo: EKCalendarChooser) {
         self.dismiss(animated:true)
     }
     
-    func calendarChooserDidFinish(_ chooser: EKCalendarChooser) {
-        // up to us to respond
-        let cals = chooser.selectedCalendars
-        guard cals.count > 0 else {
-            self.dismiss(animated:true)
-            return
-        }
+    func calendarChooserDidFinish(_ choo: EKCalendarChooser) {
+        let cals = choo.selectedCalendars
+        guard cals.count > 0 else { self.dismiss(animated:true); return }
         let calsToDelete = cals.map {$0.calendarIdentifier}
-        let alert = UIAlertController(title: "Delete selected calendar?", message: nil, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        alert.addAction(UIAlertAction(title: "Delete", style: .destructive) {
-            _ in
+        let alert = UIAlertController(title:"Delete selected calendar?",
+                                      message:nil, preferredStyle:.actionSheet)
+        alert.addAction(UIAlertAction(title:"Cancel", style:.cancel))
+        alert.addAction(UIAlertAction(title:"Delete", style:.destructive) {_ in
             for id in calsToDelete {
                 if let cal = self.database.calendar(withIdentifier:id) {
                     try? self.database.removeCalendar(cal, commit: true)
                 }
             }
-            // dismiss *everything*
-            self.dismiss(animated:true)
+            self.dismiss(animated:true) // dismiss *everything*
         })
         // alert sheet inside presented-or-popover
-        chooser.present(alert, animated: true)
+        choo.present(alert, animated: true)
     }
 }
