c921d42003373fa678cfe6f2703d74687f0fca00
Matt Neuburg <matt@tidbits.com>
2017-08-03 07:11:34 -0700

    remove segfault preventer (they fixed it), add another key path motivator

--- bk1ch05p246keyPaths/KeyPathTest/ViewController.swift
+++ bk1ch05p246keyPaths/KeyPathTest/ViewController.swift
@@ -36,21 +36,19 @@ class ViewController: UIViewController {
             print(name)
             
             var prop = \Person.firstName
-            let proplet = prop // work around segfault
             
-            let whatname = p[keyPath:proplet] // segfault here if path is a var
+            let whatname = p[keyPath:prop]
             print(whatname)
             
             prop = \Person.lastName
-            let proplet2 = prop
 
-            let whatname2 = p[keyPath:proplet2]
+            let whatname2 = p[keyPath:prop]
             print(whatname2)
             
             let newprop = prop.appending(path: \.utf8)
             _ = newprop
 
-            p[keyPath:proplet] = "Amy"
+            p[keyPath:prop] = "Amy"
             print(p)
         }
         
@@ -58,13 +56,59 @@ class ViewController: UIViewController {
             let p = Person2()
             
             let prop = \Person2.firstName
-            let proplet = prop // work around segfault
 
-            let whatname = p[keyPath:proplet]
+            let whatname = p[keyPath:prop]
             print(whatname)
 
         }
         
+        // another example, a bit more vivid perhaps
+        
+        
+        let v1 = self.view!
+        v1.backgroundColor = .red
+        let v2 = UIView(frame:.zero)
+        v2.backgroundColor = .green
+        v2.translatesAutoresizingMaskIntoConstraints = false
+        v1.addSubview(v2)
+        
+        let which = 1
+        switch which {
+        case 0:
+            let c1 = v2.topAnchor.constraint(equalTo:v1.topAnchor)
+            c1.isActive = true
+            let c2 = v2.bottomAnchor.constraint(equalTo:v1.bottomAnchor)
+            c2.isActive = true
+            let c3 = v2.leadingAnchor.constraint(equalTo:v1.leadingAnchor)
+            c3.isActive = true
+            let c4 = v2.trailingAnchor.constraint(equalTo:v1.trailingAnchor)
+            c4.isActive = true
+        case 1:
+            // failed experiments
+//            let kp : KeyPath<Any,Any> = \UIView.topAnchor
+//            let arr : [KeyPath<UIView,Any>] = [
+//                \UIView.topAnchor
+//            ]
+            let arr : [PartialKeyPath<UIView>] = [
+                \UIView.topAnchor, \UIView.bottomAnchor,
+                \UIView.leadingAnchor, \UIView.trailingAnchor
+            ]
+            for anch in arr {
+                print(type(of:anch).valueType)
+            }
+            // but I think the experiment is doomed to failure because of the odd way
+            // in which anchor types relate
+            for anch in [\UIView.topAnchor, \UIView.bottomAnchor] {
+                let c = v2[keyPath:anch].constraint(equalTo:v1[keyPath:anch])
+                c.isActive = true
+            }
+            for anch in [\UIView.leadingAnchor, \UIView.trailingAnchor] {
+                let c = v2[keyPath:anch].constraint(equalTo:v1[keyPath:anch])
+                c.isActive = true
+            }
+        default: break
+        }
+                
 
         
         
