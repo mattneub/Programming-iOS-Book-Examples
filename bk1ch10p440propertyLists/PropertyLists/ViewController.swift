7b19c803cebde73e91018e84a5e2fc12ebe53b19
Matt Neuburg <matt@tidbits.com>
2017-08-06 12:24:56 -0700

    example of Codable (property list)

---
+++ bk1ch10p440propertyLists/PropertyLists/ViewController.swift
@@ -0,0 +1,81 @@
+
+
+import UIKit
+
+struct Person : Codable {
+    let firstName : String
+    let lastName : String
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let arr = ["Manny", "Moe", "Jack"]
+        let fm = FileManager.default
+        let temp = fm.temporaryDirectory
+        let f = temp.appendingPathComponent("pep.plist")
+        (arr as NSArray).write(to: f, atomically: true)
+        // now let's see what we wrote
+        let s = try! String.init(contentsOf: f)
+        print(s)
+        
+        do {
+            let fm = FileManager.default
+            let temp = fm.temporaryDirectory
+            let f = temp.appendingPathComponent("pep.plist")
+            let arr = ["Manny", "Moe", "Jack"]
+            let penc = PropertyListEncoder()
+            penc.outputFormat = .xml
+            let d = try! penc.encode(arr)
+            try! d.write(to: f)
+            //let s = String(data:d, encoding:.utf8)
+            //print(s)
+            
+            // now let's see what we wrote
+            let s = try! String.init(contentsOf: f)
+            print(s)
+
+        }
+        
+        do {
+            let penc = PropertyListEncoder()
+            penc.outputFormat = .xml
+            let d = try! penc.encode(IndexSet([1,2,3]))
+            let s = String(data:d, encoding:.utf8)!
+            print(s)
+            
+            let ix = try! PropertyListDecoder().decode(IndexSet.self, from: d)
+            print(Array(ix))
+        }
+        
+        do {
+            let p = Person(firstName: "Matt", lastName: "Neuburg")
+            let penc = PropertyListEncoder()
+            penc.outputFormat = .xml
+            let d = try! penc.encode(p)
+            let s = String(data:d, encoding:.utf8)!
+            print(s)
+            
+            let p2 = try! PropertyListDecoder().decode(Person.self, from: d)
+            print(p2)
+
+        }
+        
+        do {
+            let p = Person(firstName: "Matt", lastName: "Neuburg")
+            let penc = PropertyListEncoder()
+            penc.outputFormat = .xml
+            let d = try! penc.encode([p])
+            let s = String(data:d, encoding:.utf8)!
+            print(s)
+            
+        }
+
+
+    
+    }
+
+}
+
