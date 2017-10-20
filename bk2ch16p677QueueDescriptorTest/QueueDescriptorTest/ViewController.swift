6a548261f0569f71e50dd53aae374bc09c41b788
Matt Neuburg <matt@tidbits.com>
2017-10-20 10:51:15 -0700

    still desperately trying to get remote control events to work on the application queue player

--- bk2ch16p677QueueDescriptorTest/QueueDescriptorTest/ViewController.swift
+++ bk2ch16p677QueueDescriptorTest/QueueDescriptorTest/ViewController.swift
@@ -19,6 +19,9 @@ func checkForMusicLibraryAccess(andThen f:(()->())? = nil) {
     case .restricted:
         // do nothing
         break
+        // just testing the syntax; this is how you get to the settings app
+        let url = URL(string:UIApplicationOpenSettingsURLString)!
+        UIApplication.shared.open(url)
     case .denied:
         // do nothing, or beg the user to authorize us in Settings
         break
@@ -28,20 +31,61 @@ func checkForMusicLibraryAccess(andThen f:(()->())? = nil) {
 
 class ViewController: UIViewController {
     
+    override func remoteControlReceived(with event: UIEvent?) {
+        print("remote!")
+    }
+    
+    override var canBecomeFirstResponder: Bool { return true }
+    
+    @IBAction func doStop(_ sender: Any) {
+        let player = MPMusicPlayerController.applicationQueuePlayer
+        player.stop()
+    }
+    
+    @objc func doPause(_ event:MPRemoteCommandEvent) {
+    }
     
     override func viewDidAppear(_ animated:Bool) {
         super.viewDidAppear(animated)
         checkForMusicLibraryAccess {
+            
+            // proving that control center never sees us as remote target
+            
+            // this doesn't help
+            self.becomeFirstResponder()
+            UIApplication.shared.beginReceivingRemoteControlEvents()
+            
+            // this doesn't help
+            try? AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try? AVAudioSession.sharedInstance().setActive(true)
+            
+            // this doesn't help
+            let scc = MPRemoteCommandCenter.shared()
+            scc.pauseCommand.addTarget(self, action:#selector(self.doPause))
+            
+            // this doesn't help
+            MPMusicPlayerController.systemMusicPlayer.stop()
+
+            // so under what circumstances are we ever the remote target?
+            
             let query = MPMediaQuery.songs()
             let result = query.items
             guard let items = result, items.count > 0 else {return}
             let song = items[0]
             
+            // this has no effect either
+            let mpic = MPNowPlayingInfoCenter.default()
+            mpic.nowPlayingInfo = [
+                MPMediaItemPropertyArtist: song.artist!,
+                MPMediaItemPropertyTitle: song.title!,
+            ]
+
+            
             let player = MPMusicPlayerController.applicationQueuePlayer
             
             // proving that MPMusicPlayerMediaItemQueueDescriptor(itemCollection:) is broken
             
-            let useCollection = true
+            let useCollection = false
             
             if useCollection {
                 let coll = MPMediaItemCollection(items: [song])
@@ -55,8 +99,6 @@ class ViewController: UIViewController {
                 let q = MPMusicPlayerMediaItemQueueDescriptor(query: query)
                 player.setQueue(with: q)
             }
-            
-            
             
             player.prepareToPlay { err in
                 if let err = err {
