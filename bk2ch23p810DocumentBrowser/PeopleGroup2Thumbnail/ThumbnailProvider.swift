5011bb825a50fd347501f3e6e97af0ed9062d706
Matt Neuburg <matt@tidbits.com>
2017-11-07 08:09:04 -0800

    add thumnail extension

---
+++ bk2ch23p810DocumentBrowser/PeopleGroup2Thumbnail/ThumbnailProvider.swift
@@ -0,0 +1,41 @@
+//
+//  ThumbnailProvider.swift
+//  PeopleGroup2Thumbnail
+//
+//  Created by Matt Neuburg on 11/7/17.
+//  Copyright © 2017 Matt Neuburg. All rights reserved.
+//
+
+import UIKit
+import QuickLook
+
+class ThumbnailProvider: QLThumbnailProvider {
+    
+    override func provideThumbnail(for request: QLFileThumbnailRequest, _ handler: @escaping (QLThumbnailReply?, Error?) -> Void) {
+        
+        // There are three ways to provide a thumbnail through a QLThumbnailReply. Only one of them should be used.
+        
+        // First way: Draw the thumbnail into the current context, set up with UIKit's coordinate system.
+        handler(QLThumbnailReply(contextSize: request.maximumSize, currentContextDrawing: { () -> Bool in
+            // Draw the thumbnail here.
+            
+            // Return true if the thumbnail was successfully drawn inside this block.
+            return true
+        }), nil)
+        
+        /*
+        
+        // Second way: Draw the thumbnail into a context passed to your block, set up with Core Graphics' coordinate system.
+        handler(QLThumbnailReply(contextSize: request.maximumSize, drawing: { (context) -> Bool in
+            // Draw the thumbnail here.
+         
+            // Return true if the thumbnail was successfully drawn inside this block.
+            return true
+        }), nil)
+         
+        // Third way: Set an image file URL.
+        handler(QLThumbnailReply(imageFileURL: Bundle.main.url(forResource: "fileThumbnail", withExtension: "jpg")!), nil)
+        
+        */
+    }
+}
