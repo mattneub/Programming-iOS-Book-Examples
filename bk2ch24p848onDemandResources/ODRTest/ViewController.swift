f35171bc9e418853538ce52046645e80287d5480
Matt Neuburg <matt@tidbits.com>
2017-11-14 18:54:40 -0800

    try to be a bit more sophisticated about NSBundleResourceRequest management

--- bk2ch24p848onDemandResources/ODRTest/ViewController.swift
+++ bk2ch24p848onDemandResources/ODRTest/ViewController.swift
@@ -3,10 +3,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var tubbyRequest : NSBundleResourceRequest?
     @IBOutlet var iv : UIImageView!
     
-    var obs = Set<NSKeyValueObservation>()
+    var requests = [Set<String>:NSBundleResourceRequest]()
 
     @IBAction func testForTubby() {
         let im = UIImage(named:"tubby")
@@ -14,32 +13,41 @@ class ViewController: UIViewController {
         let c2 = NSDataAsset(name: "control2")
         print("control2 is", c2 as Any)
         print(Bundle.main.url(forResource: "control", withExtension: "mp3") as Any)
-        print(tubbyRequest?.bundle.url(forResource: "control", withExtension: "mp3") as Any)
-        print("frac is", self.tubbyRequest?.progress.fractionCompleted as Any)
+        print(self.requests[["tubby"]]?.bundle.url(forResource: "control", withExtension: "mp3") as Any)
+        print("frac is", self.self.requests[["tubby"]]?.progress.fractionCompleted as Any)
     }
     
-    @IBAction func startUsingTubby() {
-        guard self.tubbyRequest == nil else {return}
-        self.tubbyRequest = NSBundleResourceRequest(tags: ["tubby"])
-        let ob = self.tubbyRequest!.observe(\.progress.fractionCompleted, options: .new) { req, ch in
-            print(ch)
+    // this sort of thing is probably a good idea...
+    func ensureAccessToTags(_ tags:Set<String>, andThen f:@escaping ()->()) {
+        if self.requests[tags] != nil {
+            // we have access already, just do whatever it is
+            f()
+            return
         }
-        self.obs.insert(ob)
-        self.tubbyRequest!.beginAccessingResources { err in
+        // we don't have access; provide it
+        requests[tags] = NSBundleResourceRequest(tags: tags)
+        requests[tags]!.beginAccessingResources { err in
             guard err == nil else {print(err as Any); return}
             DispatchQueue.main.async {
-                let im = UIImage(named:"tubby")
-                self.iv.image = im
+                f()
             }
         }
     }
     
+    @IBAction func startUsingTubby() {
+        self.ensureAccessToTags(["tubby"]) {
+            print("accessing tubby")
+            let im = UIImage(named:"tubby")
+            self.iv.image = im
+        }
+    }
+    
     @IBAction func stopUsingTubby() {
-        guard self.tubbyRequest != nil else {return}
-        self.iv.image = nil
-        self.tubbyRequest!.endAccessingResources()
-        self.tubbyRequest!.removeObserver(self, forKeyPath: #keyPath(NSBundleResourceRequest.progress.fractionCompleted))
-        self.tubbyRequest = nil
+        if let req = self.requests[["tubby"]] {
+            self.iv.image = nil
+            req.endAccessingResources()
+            self.requests[["tubby"]] = nil
+        }
     }
     
     
