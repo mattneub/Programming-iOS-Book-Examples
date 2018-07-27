8ade3a2c9eea3b60267d075fc90ddf797092e4b4
Matt Neuburg <matt@tidbits.com>
2018-07-26 18:36:36 -0700

    small logging example to show how a table view autoscrolls when the user taps in a text field

---
+++ bk2ch10p520tableAutoscrollingTest/TableAutoscrollingTest/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  TableAutoscrollingTest
+//
+//  Created by Matt Neuburg on 7/26/18.
+//  Copyright © 2018 Matt Neuburg. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
