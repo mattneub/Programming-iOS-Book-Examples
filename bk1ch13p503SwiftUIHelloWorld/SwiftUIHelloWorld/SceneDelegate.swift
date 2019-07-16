19c8b19778a4a3d812275a815a6b11a8f241bdb9
Matt Neuburg <matt@tidbits.com>
2019-07-15 18:01:13 -0700

    fix chapter 13 example naming, add tiny SwiftUI example

---
+++ bk1ch13p503SwiftUIHelloWorld/SwiftUIHelloWorld/SceneDelegate.swift
@@ -0,0 +1,22 @@
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use a UIHostingController as window root view controller
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: ContentView())
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+
+}
+
