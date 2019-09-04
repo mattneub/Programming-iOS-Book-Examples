381590e817c2fbc66e4cc371a80e38c73beaa31e
Matt Neuburg <matt@tidbits.com>
2019-09-05 10:25:02 +1200

    minor tweaks

--- bk2ch08p467AnimationTest/AnimationTest/AppDelegate.swift
+++ bk2ch08p467AnimationTest/AnimationTest/AppDelegate.swift
@@ -1,10 +1,4 @@
-//
-//  AppDelegate.swift
-//  AnimationTest
-//
-//  Created by Matt Neuburg on 9/5/19.
-//  Copyright © 2019 Matt Neuburg. All rights reserved.
-//
+
 
 import UIKit
 
@@ -17,21 +11,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
