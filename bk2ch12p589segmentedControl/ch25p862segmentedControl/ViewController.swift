e16c574d0293d83690f1fbcf39daf551f4b365d1
Matt Neuburg <matt@tidbits.com>
2019-09-28 19:28:19 -0700

    continuing to update chapter 12 examples

--- bk2ch12p589segmentedControl/ch25p862segmentedControl/ViewController.swift
+++ bk2ch12p589segmentedControl/ch25p862segmentedControl/ViewController.swift
@@ -37,25 +37,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.seg.layer.speed = 0.2
+        // no need to play with animation in iOS 13; the change is animated by default
+        //self.seg.layer.speed = 0.2
         delay(1) {
-            UIView.animate(withDuration:0.4) {
+            //UIView.animate(withDuration:0.4) {
                 self.seg.selectedSegmentIndex = 1
-            }
+                // self.seg.selectedSegmentIndex = UISegmentedControl.noSegment
+            //}
         }
-
+        
+        // in iOS 13, this is painted in a new way
+        // the tint color does not affect the text or the border or anything
+        // there is a selected segment tint color that is ...
+        // the background of the new sliding round rect
 //        self.seg.tintColor = .red
-//        return
+//        if #available(iOS 13.0, *) {
+//            // self.seg.selectedSegmentTintColor = .green
+//        }
+//        // self.seg.isMomentary = true
+//        self.seg.setEnabled(false, forSegmentAt: 2)
+        // return
         
         // background, set desired height but make width resizable
         // sufficient to set for Normal only
         
         let sz = CGSize(100,60)
+        let linen = UIImage(named:"linen")!
         let im = UIGraphicsImageRenderer(size:sz).image {_ in
-            UIImage(named:"linen")!.draw(in:CGRect(origin: .zero, size: sz))
+            linen.draw(in:CGRect(origin: .zero, size: sz))
             }.resizableImage(withCapInsets:
                 UIEdgeInsets(top: 0,left: 10,bottom: 0,right: 10), resizingMode: .stretch)
         self.seg.setBackgroundImage(im, for:.normal, barMetrics: .default)
+        // okay but in iOS 13 it is NOT sufficient to set for Normal only
+        let im2 = UIGraphicsImageRenderer(size:sz).image {ctx in
+            let r = CGRect(origin: .zero, size: sz)
+            ctx.cgContext.setFillColor(UIColor.blue.withAlphaComponent(0.1).cgColor)
+            ctx.cgContext.fill(r)
+            linen.draw(in: r, blendMode: .destinationAtop, alpha: 1)
+            }.resizableImage(withCapInsets:
+                UIEdgeInsets(top: 0,left: 10,bottom: 0,right: 10), resizingMode: .stretch)
+        self.seg.setBackgroundImage(im2, for:.selected, barMetrics: .default)
         
         // segment images, redraw at final size
         let pep = ["manny", "moe", "jack"]
@@ -81,16 +102,26 @@ class ViewController: UIViewController {
                 UIImage(named:"smiley")!.withRenderingMode(.alwaysOriginal),
                 "Two"
             ])
+        seg.tintColor = .red // no effect in iOS 13
+        if #available(iOS 13.0, *) {
+            // this works in iOS 13 to tint the image
+            seg.setImage(UIImage(named:"smiley")!
+                .withRenderingMode(.alwaysOriginal)
+                .withTintColor(.green),
+                         forSegmentAt: 0)
+            print(seg.tintColor == .red) // true but so what?
+        }
         // yay, keys work
         seg.setTitleTextAttributes([
-            .foregroundColor: UIColor.black
+            .foregroundColor: UIColor.blue // this works in iOS 13 to color the text
+            // whoa! more than that, it is the default tint color for template images too!!!!!!!!
         ], for: .normal)
+
         seg.frame.origin = CGPoint(40,100)
         seg.frame.size.width = 200
         //seg.setWidth(0, forSegmentAt: 0)
         seg.apportionsSegmentWidthsByContent = true
         seg.sizeToFit()
-        seg.tintColor = .red
         self.view.addSubview(seg)
         return;
         seg.translatesAutoresizingMaskIntoConstraints = false
@@ -99,5 +130,11 @@ class ViewController: UIViewController {
             seg.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 40),
             seg.heightAnchor.constraint(equalToConstant: UIImage(named:"smiley")!.size.height)
             ])
+    }
+    
+    @IBAction func didSelect(_ sender:UISegmentedControl) {
+        delay(0.1) {
+            print("select", sender.selectedSegmentIndex)
+        }
     }
 }
--- bk2ch12p595button/ch25p866button/ViewController.swift
+++ bk2ch12p595button/ch25p866button/ViewController.swift
@@ -45,6 +45,13 @@ class ViewController: UIViewController {
         self.button2.titleLabel!.textAlignment = .center
         self.button2.setTitle("Button with a title that wraps", for:.normal)
         
+        do {
+            let font = UIFont.systemFont(ofSize: 40)
+            let config = UIImage.SymbolConfiguration(font: font)
+            // legal, oddly
+            self.button2.setPreferredSymbolConfiguration(config, forImageIn: .normal)
+        }
+        
         // showing how the image drives out the title if it's too large
         do {
             var shrinkImage : Bool { return false } // change to true to shrink the image and show the title
@@ -56,6 +63,57 @@ class ViewController: UIViewController {
                 self.button3.setImage(im, for:.normal)
             }
         }
+        
+        // new in iOS 13
+        // you _can_ use a non symbol image, but don't
+        let sys = UIButton.systemButton(with: UIImage(systemName: "smiley")!, target: nil, action: nil)
+        sys.tintColor = .red
+        let font = UIFont.systemFont(ofSize: 40)
+        let config = UIImage.SymbolConfiguration(font: font)
+        sys.setPreferredSymbolConfiguration(config, forImageIn: .normal)
+        // so you _can_ add text...
+        // but it doesn't magically conform to the _symbol_ configuration
+        sys.setTitle("Test", for: .normal)
+        // so let's make them conform to one another
+        sys.titleLabel?.font = font
+        sys.sizeToFit() // crucial, or use autolayout
+        sys.frame.origin = CGPoint(50, 350)
+        sys.backgroundColor = .white
+        self.view.addSubview(sys)
+        
+        // so what happens if you do that the old way?
+        // it's exactly the same
+        do {
+            let sys = UIButton(type: .system)
+            sys.setImage(UIImage(systemName: "smiley")!, for:.normal)
+            sys.tintColor = .red
+            let font = UIFont.systemFont(ofSize: 40)
+            let config = UIImage.SymbolConfiguration(font: font)
+            sys.setPreferredSymbolConfiguration(config, forImageIn: .normal)
+            // so you _can_ add text...
+            // but it doesn't magically conform to the _symbol_ configuration
+            sys.setTitle("Test", for: .normal)
+            // so let's make them conform to one another
+            sys.titleLabel?.font = font
+            sys.sizeToFit() // crucial, or use autolayout
+            sys.frame.origin = CGPoint(250, 350)
+            sys.backgroundColor = .white
+            self.view.addSubview(sys)
+
+        }
+        
+        let rr = UIButton(type: .close) // new type
+        rr.setTitle("Test", for: .normal)
+        rr.frame.origin = CGPoint(50,400)
+        rr.sizeToFit()
+        rr.tintColor = .red // no effect
+        print(rr.currentBackgroundImage) // circle
+        print(rr.currentImage) // xmark symbol image
+        //rr.backgroundColor = .white
+        rr.imageView?.tintColor = .green // nope
+        self.view.addSubview(rr)
+        
+        // self.view.backgroundColor = .white
     }
 
 
