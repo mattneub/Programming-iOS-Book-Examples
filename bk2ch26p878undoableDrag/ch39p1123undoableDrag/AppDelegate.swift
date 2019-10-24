9eecb37f54885701ea64a3e597b62633969e7ede
Matt Neuburg <matt@tidbits.com>
2019-10-24 11:20:26 -0700

    use nil targeted action, cooler

--- bk2ch26p878undoableDrag/ch39p1123undoableDrag/AppDelegate.swift
+++ bk2ch26p878undoableDrag/ch39p1123undoableDrag/AppDelegate.swift
@@ -6,12 +6,10 @@ class AppDelegate : UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
         self.window?.backgroundColor = .white
-        return true // uncomment to remove test buttons
+        // return true // uncomment to remove test buttons
         let vc = UIStoryboard(name:"Main", bundle:nil).instantiateInitialViewController()!
-        vc.loadViewIfNeeded()
-        let red = vc.view.subviews[0] as! MyView
-        let undoButton = UIBarButtonItem(title: "Undo", style: .plain, target: red, action: #selector(MyView.undo(_:)))
-        let redoButton = UIBarButtonItem(title: "Redo", style: .plain, target: red, action: #selector(MyView.redo(_:)))
+        let undoButton = UIBarButtonItem(title: "Undo", style: .plain, target: nil, action: #selector(MyView.undo))
+        let redoButton = UIBarButtonItem(title: "Redo", style: .plain, target: nil, action: #selector(MyView.redo))
         vc.navigationItem.rightBarButtonItems = [undoButton, redoButton]
         self.window?.rootViewController = UINavigationController(rootViewController:vc)
         return true
