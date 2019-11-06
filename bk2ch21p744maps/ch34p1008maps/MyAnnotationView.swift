98186f2bd898d5b8ef925fcec6caa76e4104cc6e
Matt Neuburg <matt@tidbits.com>
2019-11-06 10:14:23 -0800

    note to self

--- bk2ch21p744maps/ch34p1008maps/MyAnnotationView.swift
+++ bk2ch21p744maps/ch34p1008maps/MyAnnotationView.swift
@@ -8,6 +8,7 @@ class MyBikeAnnotationView : MKAnnotationView {
         willSet {
             self.image = UIImage(named:"clipartdirtbike.gif")
             // why do I have to change this? suddenly we have to take scale into account?
+            // and why does this work differently on device (right) from simulator (wrong)?
             let scale = UIScreen.main.scale
             self.bounds.size.height /= 3.0 / scale
             self.bounds.size.width /= 3.0 / scale
