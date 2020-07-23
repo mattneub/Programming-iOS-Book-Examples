9fc064f1374927a193df65959c6fec69dad40232
matt neuburg <matt@tidbits.com>
2020-07-23 10:51:41 -0700

    minor mistake corrections

--- bk2ch09p488basicMasterDetail/MasterDetail/AppDelegate.swift
+++ bk2ch09p488basicMasterDetail/MasterDetail/AppDelegate.swift
@@ -9,6 +9,10 @@ class AppDelegate : UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
     
+        if #available(iOS 13.0, *) {
+            return true
+        }
+
         self.window = self.window ?? UIWindow()
 
         let svc = UISplitViewController()
