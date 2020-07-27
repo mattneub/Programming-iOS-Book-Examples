6f7aafa4add743f0ebe0dcac26bce6727f7e1dcb
matt neuburg <matt@tidbits.com>
2020-07-26 19:44:15 -0700

    I see now, this is how you're supposed to do it

--- bk2ch08p408selfConfiguringCell2/SelfConfiguringCell/MyCell.swift
+++ bk2ch08p408selfConfiguringCell2/SelfConfiguringCell/MyCell.swift
@@ -6,14 +6,45 @@ class MyCell : UITableViewCell {
     @IBOutlet var theLabel : UILabel!
     @IBOutlet var theImageView : UIImageView!
 
-    class MyContentView: UIView & UIContentView {
-        var configuration: UIContentConfiguration
+    class MyContentView: UIView, UIContentView {
+        var configuration: UIContentConfiguration {
+            didSet {
+                self.configure()
+            }
+        }
+        private let lab = UILabel()
+        private let iv = UIImageView()
         init(configuration: UIContentConfiguration) {
             self.configuration = configuration
             super.init(frame: .zero)
+            self.lab.numberOfLines = 2
+            self.lab.font = UIFont(name: "Helvetica-Bold", size: 16)
+            self.iv.contentMode = .center
+            // construct the view
+            self.addSubview(self.lab)
+            self.addSubview(self.iv)
+            self.lab.translatesAutoresizingMaskIntoConstraints = false
+            self.iv.translatesAutoresizingMaskIntoConstraints = false
+            self.lab.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant:15).isActive = true
+            self.lab.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+            self.lab.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+            self.iv.leadingAnchor.constraint(equalTo: self.lab.trailingAnchor, constant: 15).isActive = true
+            self.iv.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+            self.iv.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+            self.iv.widthAnchor.constraint(equalTo: self.iv.heightAnchor).isActive = true
         }
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
+        }
+        private func configure() {
+            guard let config = self.configuration as? Configuration else { return }
+            self.lab.text = config.text
+            let im = config.image
+            let r = UIGraphicsImageRenderer(size:CGSize(36,36), format:im.imageRendererFormat)
+            let im2 = r.image {
+                _ in im.draw(in:CGRect(0,0,36,36))
+            }
+            self.iv.image = im2
         }
     }
     struct Configuration : UIContentConfiguration {
@@ -21,35 +52,10 @@ class MyCell : UITableViewCell {
         let image: UIImage
         // this view will automatically _replace_ the cell's `contentView`
         func makeContentView() -> UIView & UIContentView {
-            let lab = UILabel()
-            lab.numberOfLines = 2
-            lab.font = UIFont(name: "Helvetica-Bold", size: 16)
-            lab.text = self.text
-            let iv = UIImageView()
-            iv.contentMode = .center
-            let im = self.image
-            let r = UIGraphicsImageRenderer(size:CGSize(36,36), format:im.imageRendererFormat)
-            let im2 = r.image {
-                _ in im.draw(in:CGRect(0,0,36,36))
-            }
-            iv.image = im2
-            // construct the view
-            let c = MyContentView(configuration: self)
-            c.addSubview(lab)
-            c.addSubview(iv)
-            lab.translatesAutoresizingMaskIntoConstraints = false
-            iv.translatesAutoresizingMaskIntoConstraints = false
-            lab.leadingAnchor.constraint(equalTo: c.leadingAnchor, constant:15).isActive = true
-            lab.topAnchor.constraint(equalTo: c.topAnchor).isActive = true
-            lab.bottomAnchor.constraint(equalTo: c.bottomAnchor).isActive = true
-            iv.leadingAnchor.constraint(equalTo: lab.trailingAnchor, constant: 15).isActive = true
-            iv.trailingAnchor.constraint(equalTo: c.trailingAnchor, constant: -15).isActive = true
-            iv.centerYAnchor.constraint(equalTo: c.centerYAnchor).isActive = true
-            iv.widthAnchor.constraint(equalTo: iv.heightAnchor).isActive = true
-            return c
+            return MyContentView(configuration: self)
         }
-        func updated(for state: UIConfigurationState) -> MyCell.Configuration {
-            print("updating for state", state)
+        func updated(for state: UIConfigurationState) -> Self {
+            // we have no state-based changes, just return self
             return self
         }
     }
