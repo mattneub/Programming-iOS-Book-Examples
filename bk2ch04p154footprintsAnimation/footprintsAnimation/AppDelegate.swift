bd6fe92d136f8854079555ba961c87adddd7702d
Matt Neuburg <matt@tidbits.com>
2020-09-01 20:13:45 -0700

    footprints example

---
+++ bk2ch04p154footprintsAnimation/footprintsAnimation/AppDelegate.swift
@@ -0,0 +1,16 @@
+
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ bk2ch04p154footprintsAnimation/footprintsAnimation/SceneDelegate.swift
@@ -0,0 +1,10 @@
+
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
+
