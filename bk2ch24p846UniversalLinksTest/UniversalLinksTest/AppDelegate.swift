c593493916dee1d0315a1cc0915ce04cbebaa17a
Matt Neuburg <matt@tidbits.com>
2021-05-13 19:09:13 +1200

    more logging on universal links example

--- bk2ch24p846UniversalLinksTest/UniversalLinksTest/AppDelegate.swift
+++ bk2ch24p846UniversalLinksTest/UniversalLinksTest/AppDelegate.swift
@@ -5,8 +5,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
--- bk2ch24p846UniversalLinksTest/UniversalLinksTest/SceneDelegate.swift
+++ bk2ch24p846UniversalLinksTest/UniversalLinksTest/SceneDelegate.swift
@@ -3,8 +3,15 @@
 import UIKit
 import os
 
+@available(iOS 14.0, *)
 let logger = Logger(subsystem: "universalLinksTest", category: "link")
 
+func delay(_ delay:Double, closure:@escaping ()->()) {
+    let when = DispatchTime.now() + delay
+    DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
+}
+
+@available(iOS 14.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -16,13 +23,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        logger.log("launching")
         if let url = connectionOptions.userActivities.first?.webpageURL {
             logger.log("got universal link on launch: \(url.path)")
+            delay(2) {
+                self.show(url)
+            }
         }
     }
     
     func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
         if let url = userActivity.webpageURL {
+            logger.log("continuing")
             self.show(url)
         }
     }
