a6974da2d8205ea038cafee8dba2abe4dcb45ec3
Matt Neuburg <matt@tidbits.com>
2021-09-08 06:46:18 -0700

    prove that autoreleasepool works in a task

--- bk1ch12p490autoreleasepool/bk1ch12p490autoreleasepool/ViewController.swift
+++ bk1ch12p490autoreleasepool/bk1ch12p490autoreleasepool/ViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let which = 2
+    let which = 3
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -11,6 +11,10 @@ class ViewController: UIViewController {
         switch which {
         case 1: self.test()
         case 2: self.test2()
+        case 3:
+            Task {
+                await self.test3()
+            }
         default: break
         }
     }
@@ -27,6 +31,18 @@ class ViewController: UIViewController {
     }
     
     func test2() {
+        let path = Bundle.main.path(forResource: "001", ofType: "png")!
+        for j in 0 ..< 50 {
+            autoreleasepool {
+                for i in 0 ..< 100 {
+                    let im = UIImage(contentsOfFile: path)
+                }
+            }
+        }
+    }
+    
+    // works fine in an async function
+    func test3() async {
         let path = Bundle.main.path(forResource: "001", ofType: "png")!
         for j in 0 ..< 50 {
             autoreleasepool {
