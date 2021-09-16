80c1974b3fa60a0925df86bfb1a504abc04dda58
Matt Neuburg <matt@tidbits.com>
2021-09-16 15:03:10 -0700

    comment

--- bk1ch05zp05MakeYourOwnAsyncStream/MakeYourOwnAsyncStream/ViewController.swift
+++ bk1ch05zp05MakeYourOwnAsyncStream/MakeYourOwnAsyncStream/ViewController.swift
@@ -14,6 +14,7 @@ class TextFieldSelectionChangeStreamer: NSObject, UITextFieldDelegate {
             continuation.onTermination = { term in
                 print("terminated!")
             } as (@Sendable (AsyncStream<UITextField>.Continuation.Termination) -> Void)
+            // but not fixed in RC
         }
         super.init()
         self.continuation = myContinuation
